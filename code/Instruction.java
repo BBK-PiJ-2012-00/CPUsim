27e59baef0a84fd8f672ac4459c96a2a62281c96
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-07 18:03:47 +0100

    Added more comments to Instruction, as well as ensuring the override of toString() for all classes which implement Instruction.

--- code/Instruction.java
+++ code/Instruction.java
@@ -1,11 +1,14 @@
 package code;
 
 /*
- * Instructions are 32 bits in length.  Representation is initiall in hexadecimal, but to ensure integrity,
+ * All instructions implement the Instruction interface, which allows for code reuse and ensures all instructions implement
+ * the same key methods.
+ * 
+ * Instructions are 32 bits in length.  Representation is initially in hexadecimal, but to ensure integrity,
  * hexadecimal values for the instruction fields must not exceed the maximum value possible for a given length of bits.
  * Opcodes currently occupy the first 4 bits of an instruction, permitting no more than 16 opcodes for the instruction set.
  * Whilst the simulator would work fine if instruction fields were allowed to overflow their bit allocation, it would not
- * be very realistic. 
+ * be very realistic.  The same applies to other fields within an instruction. 
  */
 
 public interface Instruction {
@@ -21,6 +24,9 @@ public interface Instruction {
 	 * @return String hexadecimal machine code representation of instruction
 	 */
 	public String toMachineString();
+	
+	@Override
+	public String toString();
 
 }
 
