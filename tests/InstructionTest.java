20ce35c6940bcf97cca1903e4c63f3c70619117d
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-07 17:15:35 +0100

    Creation of first TransferInstr type successful.

--- tests/InstructionTest.java
+++ tests/InstructionTest.java
@@ -1,5 +1,9 @@
 package tests;
 
+import code.Instruction;
+import code.Opcode;
+import code.TransferInstr;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -7,8 +11,12 @@ import org.junit.Test;
 public class InstructionTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void TransferInstructionTest() {
+		Instruction instr = new TransferInstr(Opcode.MOVE, 0, 0);
+		Opcode expected = Opcode.MOVE;
+		Opcode output = instr.getOpcode();
+		assertEquals(expected, output);
+		
 	}
 
 }
