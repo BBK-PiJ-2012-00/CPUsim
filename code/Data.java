7c4255719e1add869c0ecd26fdb27af8e751e53a
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-08 15:58:51 +0100

    Removed Operand interface; unnecessary.

--- code/Data.java
+++ code/Data.java
@@ -3,6 +3,9 @@ package code;
 /*
  * Main memory is implemented as an array of type Data. Instructions implement data, as does anything else
  * that is stored in main memory.
+ * 
+ * While main memory is an array of type Data, things declared as Operand and Instruction can be stored within (but
+ * come out as type Data?)
  */
 public interface Data {
 	
