45232032f75901dc51866745853568bc4ef2e595
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-08 15:58:51 +0100

    Removed Operand interface; unnecessary.

--- code/IntegerOperand.java
+++ code/IntegerOperand.java
@@ -1,6 +1,12 @@
 package code;
 
-public class IntegerOperand implements Operand {
+/*
+* A wrapper class to hold integer operands.  Implements data to allow for storage in main memory.
+* This helps manage and structure the data used in the simulator; all data extends Data at the top of the hierarchy,
+* with Instruction and Operands being on the second tier.  This avoids data in main memory being represented as type
+* Object, which is less specific. Casting isn't eliminated, but this approach is more belt and braces than using Object.
+*/
+public class IntegerOperand implements Data {
 	private int operand;
 	
 	
