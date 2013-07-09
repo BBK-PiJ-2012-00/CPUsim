6b3d4f80ed97a296f93f67bf075595cce36c451b
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-09 13:32:23 +0100

    Drafting out potential methods.

--- code/MemoryModule.java
+++ code/MemoryModule.java
@@ -1,5 +1,31 @@
 package code;
 
 public class MemoryModule implements MainMemory {
+	private Data[] memoryContents = new Data[100]; //Array representing main memory itself
+	private int pointer; //Points to next available location
+	
+	
+	@Override
+	public Instruction readInstruction() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int readInteger() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double readFloatingPoint() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public void writeInstruction(Instruction instr, int index) { //unclear if index should be explicitly specified, or done using 
+																//pointer field
+		
+	}
 
 }
