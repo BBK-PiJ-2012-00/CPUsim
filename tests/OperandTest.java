39cad93111552a150e280d2b6b06e2d127e67c01
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-10 13:11:13 +0100

    Testing Operand; integer operand tests successful so far.

--- tests/OperandTest.java
+++ tests/OperandTest.java
@@ -9,10 +9,33 @@ import code.Data;
 import org.junit.Test;
 
 public class OperandTest {
+	Operand intOp;
+	Operand floatOp;
 
+	/*
+	 * Tests creation of an integer operand, and that it is recognised as such.
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIntOperand1() {
+		intOp = new OperandImpl(5);
+		boolean output = intOp.isInteger();
+		assertTrue(output);
+	}
+	
+	@Test
+	public void testIntOperand2() {
+		intOp = new OperandImpl(10);
+		int output = intOp.unwrapInteger();
+		int expected = 10;
+		assertEquals(expected, output);
+	}
+	
+	@Test
+	public void testIntOperand3() { // Tests that a floating point operand is returned if requested of an integer operand
+		intOp = new OperandImpl(13);
+		double output = intOp.unwrapFloatingPoint();
+		double expected = 13.0;
+		assertEquals(expected, output, 13.0);		
 	}
 
 }
