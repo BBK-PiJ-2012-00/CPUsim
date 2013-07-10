4b430535b3e4be5210026dd82168d7b8d017b5f5
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-11 00:53:36 +0100

    Starting ArithmeticInstr testing.

--- tests/InstructionTest.java
+++ tests/InstructionTest.java
@@ -3,6 +3,7 @@ package tests;
 import code.Instruction;
 import code.Opcode;
 import code.TransferInstr;
+import code.ArithmeticInstr;
 
 import static org.junit.Assert.*;
 
@@ -12,7 +13,7 @@ public class InstructionTest {
 	Instruction instr;
 
 	@Test
-	public void TransferInstructionTest() {
+	public void transferInstructionTest() {
 		instr = new TransferInstr(Opcode.MOVE, 0, 0);
 		Opcode expected = Opcode.MOVE;
 		Opcode output = instr.getOpcode(); //validates getOpcode() method
@@ -21,7 +22,7 @@ public class InstructionTest {
 	}
 	
 	@Test 
-	public void TransferInstructionOpcodeTest() { //To test instruction not created with illegal opcode for format
+	public void transferInstructionOpcodeTest() { //To test instruction not created with illegal opcode for format
 		try {
 			instr = new TransferInstr(Opcode.ADD, 0, 0);
 		}
@@ -32,7 +33,7 @@ public class InstructionTest {
 	}
 	
 	@Test
-	public void TransferInstrMachineStringTest() {
+	public void transferInstrMachineStringTest() {
 		instr = new TransferInstr(Opcode.LOAD, 0, 0);
 		String output = instr.toMachineString();
 		String expected = "1 0 0";
@@ -41,7 +42,7 @@ public class InstructionTest {
 	}
 	
 	@Test
-	public void TransferInstrToStringTest() {
+	public void transferInstrToStringTest() {
 		instr = new TransferInstr(Opcode.STORE, 0, 0);
 		String output = instr.toString();
 		String expected = "STORE 0 0";
@@ -49,11 +50,31 @@ public class InstructionTest {
 	}
 	
 	@Test 
-	public void TransferInstrGetOpcodeTest() {
+	public void transferInstrGetOpcodeTest() {
 		instr = new TransferInstr(Opcode.MOVE, 0, 0);
 		Opcode output = instr.getOpcode();
 		Opcode expected = Opcode.MOVE;
 		assertEquals(expected, output);
 	}
+	
+	@Test
+	public void arithmeticInstrTest() {
+		instr = new ArithmeticInstr(Opcode.ADD, 10, 20);
+		Opcode output = instr.getOpcode();
+		Opcode expected = Opcode.ADD;
+		assertEquals(expected, output);
+	}
+	
+	@Test
+	public void arithmeticInstrOpcodeTest() {
+		try {
+			instr = new ArithmeticInstr(Opcode.BR, 10, 20);
+		}
+		catch (IllegalStateException e) {
+			System.out.println(e.getMessage());
+		}
+		assertNull(instr); //Checks instruction isn't created with invalid opcode for its format.
+	}
+		
 
 }
