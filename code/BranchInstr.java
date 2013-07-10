edac683e91e87b7f376e2416ca1797cc9e3462d1
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-10 15:44:15 +0100

    Created branch instruction class.

---
+++ code/BranchInstr.java
@@ -0,0 +1,18 @@
+package code;
+
+public class BranchInstr extends Instruction {
+	private int branchTarget;
+
+	@Override
+	public int getField1() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getField2() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
