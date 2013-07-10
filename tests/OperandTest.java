c83a566d468c98d984c4b9f1de2d3c884c25ee84
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-10 13:21:02 +0100

    All methods of Operand tested successfully.

--- tests/OperandTest.java
+++ tests/OperandTest.java
@@ -43,5 +43,46 @@ public class OperandTest {
 		intOp = new OperandImpl(7);
 		assertFalse(intOp.isInstruction());
 	}
+	
+	@Test
+	public void testIntOperand5() {
+		intOp = new OperandImpl(0);
+		assertFalse(intOp.isFloatingPoint());
+	}
+	
+	@Test
+	public void testFloatingPointOperand1() { //Tests creation of operand that wraps a floating point value
+		floatOp = new OperandImpl(1.5);
+		boolean output = floatOp.isFloatingPoint();
+		assertTrue(output);
+	}
+	
+	@Test
+	public void testFloatingPointOperand2() {
+		floatOp = new OperandImpl(4.321);
+		double output = floatOp.unwrapFloatingPoint();
+		double expected = 4.321;
+		assertEquals(expected, output, 0.0);
+	}
+	
+	@Test
+	public void testFloatingPointOperand3() { //Tests correct return of an integer if requested of a floating point operand
+		floatOp = new OperandImpl(6.79);
+		int output = floatOp.unwrapInteger();
+		int expected = 6;
+		assertEquals(expected, output);
+	}
+	
+	@Test
+	public void testFloatingPointOperand4() {
+		floatOp = new OperandImpl(2.2);
+		assertFalse(floatOp.isInstruction());
+	}
+	
+	@Test
+	public void testFloatingPointOperand5() {
+		floatOp = new OperandImpl(3.4);
+		assertFalse(floatOp.isInteger());
+	}
 
 }
