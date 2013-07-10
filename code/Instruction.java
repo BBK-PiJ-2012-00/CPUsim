c826d33e054f48a8b571be0eafdfaec3807b7ba5
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-10 14:30:04 +0100

    Changed Instruction from an interface to an abstract class; this vastly reduces the duplicate code between instruction implementations, as they have much code in common, and allows for code reuse.

--- code/Instruction.java
+++ code/Instruction.java
@@ -1,8 +1,8 @@
 package code;
 
 /*
- * All instructions implement the Instruction interface, which allows for code reuse and ensures all instructions implement
- * the same key methods.
+ * All instructions extend the abstract Instruction class. This allows for code reuse, reduces redundant code that would be
+ * brought about were Instruction implemented as an interface, and ensures all instructions implement the same key methods.
  * 
  * Instructions are 32 bits in length.  Representation is initially in hexadecimal, but to ensure integrity,
  * hexadecimal values for the instruction fields must not exceed the maximum value possible for a given length of bits.
@@ -11,22 +11,77 @@ package code;
  * be very realistic.  The same applies to other fields within an instruction. 
  */
 
-public interface Instruction extends Data {
+/*
+ * TO DO:
+ * 		Could flesh out toString() methods in abstract class; can use getField() methods to achieve this,
+ * 			and getOpcode() to differentiate branch instructions and use only getField1().
+ */
+
+public abstract class Instruction implements Data {
 	
-	/*
-	 * @return Opcode returns opcode field of instruction
-	 */
-	public Opcode getOpcode();
-	
-	/*
-	 * Returns hexadecimal machine code representation of instruction on screen.
-	 * 
-	 * @return String hexadecimal machine code representation of instruction
-	 */
-	public String toMachineString();
-	
+	private final Opcode OPCODE = null;
+
 	@Override
-	public String toString();
+	public boolean isInstruction() {
+		return true;
+	}
+
+	@Override
+	public boolean isInteger() {
+		return false;
+	}
+
+	@Override
+	public boolean isFloatingPoint() {
+		return false;
+	}
+	
+	/*
+	 * Returns the opcode of the instruction.
+	 * 
+	 * @return Opcode the opcode field of the instruction.
+	 */
+	public Opcode getOpcode() {
+		return this.OPCODE;
+	}
+	
+	/*
+	 * Returns instruction in String format, for printing on screen. Opcode 
+	 * is in mnemonic form. 
+	 */
+	public String toString() {
+		if (OPCODE.getValue() > 7) { //For branch instructions, which only have one instruction field besides opcode
+			return OPCODE.toString() + " " + Integer.toHexString(this.getField1());
+		}
+		return OPCODE.toString() + " " + Integer.toHexString(this.getField1()) + " " + Integer.toHexString(this.getField2());
+	}
+	
+	/*
+	 * Returns instruction in String format, in hexadecimal representation.
+	 * 
+	 * @return String hexadecimal representation of instruction.
+	 */
+	public abstract String toMachineString();
+	
+	/*
+	 * Returns the first instruction field. For data transfer instructions, this is the source location (14 bits);
+	 * for arithmetic instructions, the register whose contents is an operand in the arithmetic operation, as well
+	 * as the location where the result of the operation is to be stored (14 bits); for 
+	 * branch instructions, the branch target (28 bits).
+	 * 
+	 * @return int the integer representation of the first field of the instruction.
+	 */
+	public abstract int getField1(); //All instructions have at least one field
+	
+	/*
+	 * Returns the second field of an instruction. For branch instructions, the return value will be 0 as these instructions
+	 * have only one field (the branch target). For data transfer instructions, this field is the destination location;
+	 * for arithmetic instructions, this is the register location where one of the operands to the arithmetic operation
+	 * is stored.
+	 * 
+	 * @return int the integer representation of the second instruction field.
+	 */
+	public abstract int getField2(); //Branch instructions don't have this: can return 0, or other meaningless value.
 
 }
 
