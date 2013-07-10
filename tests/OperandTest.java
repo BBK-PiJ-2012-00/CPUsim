e8cca1999731a534bc4a9364c48b8484616d2138
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-10 13:02:31 +0100

    Initial setup; failed as expected.

---
+++ tests/OperandTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import code.Operand;
+import code.OperandImpl;
+import code.Data;
+
+import org.junit.Test;
+
+public class OperandTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
