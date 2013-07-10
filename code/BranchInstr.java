026487a720f8e2381914ae666c719d238f5fefc9
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-10 23:03:07 +0100

    Fleshed out BranchInstr class. getField2() method returns -1 to reflect absence of second field.

--- code/BranchInstr.java
+++ code/BranchInstr.java
@@ -1,18 +1,32 @@
 package code;
 
+/*
+ * TO DO:
+ * 		Place restrictions on branch target field in constructor depending on memory address size
+ */
+
 public class BranchInstr extends Instruction {
-	private int branchTarget;
+	private int branchTarget; //28-bit branch target field
+	
+	public BranchInstr(Opcode opcode, int branchTarget) {
+		super(opcode);
+		if (this.OPCODE.getValue() < 8 || this.OPCODE.getValue() > 13) {
+			throw new IllegalStateException("Illegal opcode for this instruction format!");
+		}
+		if (branchTarget > 99) { //100 memory addresses at present (0-99).
+			throw new IllegalStateException("Memory addresss out of bounds!");
+		}
+		this.branchTarget = branchTarget;
+	}
 
 	@Override
 	public int getField1() {
-		// TODO Auto-generated method stub
-		return 0;
+		return branchTarget;
 	}
 
 	@Override
 	public int getField2() {
-		// TODO Auto-generated method stub
-		return 0;
+		return -1; //Branch instructions have only one field.
 	}
 
 }
