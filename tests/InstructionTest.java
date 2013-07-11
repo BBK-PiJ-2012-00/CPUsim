f3f6c9a32cb32d6d0ef2891e39c8d20cf8dc7b94
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-11 12:38:07 +0100

    Completed initial tests on all instruction types. Further tests to follow should any additional methods or fields be required in the future.

--- tests/InstructionTest.java
+++ tests/InstructionTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import code.BranchInstr;
 import code.Instruction;
 import code.Opcode;
 import code.TransferInstr;
@@ -58,6 +59,22 @@ public class InstructionTest {
 	}
 	
 	@Test
+	public void transferInstrGetField1Test() {
+		instr = new TransferInstr(Opcode.LOAD, 50, 70);
+		int output = instr.getField1();
+		int expected = 50;
+		assertEquals(expected, output);
+	}
+	
+	@Test
+	public void transferInstrGetField2Test() {
+		instr = new TransferInstr(Opcode.STORE, 50, 70);
+		int output = instr.getField2();
+		int expected = 70;
+		assertEquals(expected, output);
+	}
+	
+	@Test
 	public void arithmeticInstrTest() {
 		instr = new ArithmeticInstr(Opcode.ADD, 10, 20);
 		Opcode output = instr.getOpcode();
@@ -100,6 +117,81 @@ public class InstructionTest {
 		Opcode expected = Opcode.MUL;
 		assertEquals(expected, output);
 	}
+	
+	@Test
+	public void arithmeticInstrGetField1Test() {
+		instr = new ArithmeticInstr(Opcode.ADD, 5, 10);
+		int output = instr.getField1();
+		int expected = 5;
+		assertEquals(expected, output);
+	}
+	
+	@Test
+	public void arithmeticInstrGetField2Test() {
+		instr = new ArithmeticInstr(Opcode.ADD, 5, 10);
+		int output = instr.getField2();
+		int expected = 10;
+		assertEquals(expected, output);
+	}
+	
+	@Test
+	public void branchInstrTest() { //Tests creation of branch instruction
+		instr = new BranchInstr(Opcode.BR, 0);
+		assertNotNull(instr);
 		
+	}
+	
+	@Test 
+	public void branchInstrInvalidOpcodeTest() { //To test instruction is not created with illegal opcode for format
+		try {
+			instr = new BranchInstr(Opcode.ADD, 0);
+		}
+		catch (IllegalStateException e) {
+			System.out.println(e.getMessage());
+		}
+		assertNull(instr); //instr should still be null if creation has failed, which it should because of invalid opcode		
+	}
+	
+	@Test
+	public void branchInstrMachineStringTest() {
+		instr = new BranchInstr(Opcode.BRE, 0);
+		String output = instr.toMachineString();
+		String expected = "10 0";
+		assertEquals(expected, output);
+		
+	}
+	
+	@Test
+	public void branchInstrToStringTest() {
+		instr = new BranchInstr(Opcode.BRNE, 0);
+		String output = instr.toString();
+		String expected = "BRNE 0";
+		assertEquals(expected, output);
+	}
+	
+	@Test 
+	public void branchInstrGetOpcodeTest() {
+		instr = new BranchInstr(Opcode.BR, 0);
+		Opcode output = instr.getOpcode();
+		Opcode expected = Opcode.BR;
+		assertEquals(expected, output);
+	}
+	
+	@Test
+	public void branchInstrGetField1Test() {
+		instr = new BranchInstr(Opcode.BRZ, 45);
+		int output = instr.getField1();
+		int expected = 45;
+		assertEquals(expected, output);
+	}
+	
+	@Test
+	public void branchInstrGetField2Test() {
+		instr = new BranchInstr(Opcode.ISZ, 70);
+		int output = instr.getField2();
+		int expected = -1; //-1 fixed return value for getField2() for branch instructions
+		assertEquals(expected, output);
+	}
+	
 
 }
