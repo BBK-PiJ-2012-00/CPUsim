9428a3654b4cb64df39932fa212aefa8ad72a92b
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-12 00:16:53 +0100

    Fleshing out MemoryModule class.

--- code/MemoryModule.java
+++ code/MemoryModule.java
@@ -6,8 +6,11 @@ public class MemoryModule implements MainMemory {
 	
 	
 	@Override
-	public Instruction readInstruction() {
-		// TODO Auto-generated method stub
+	public Instruction readInstruction(int index) {
+		if (memoryContents[index].isInstruction()) { //Check contents of address is of type Instruction
+			Instruction instr = (Instruction) memoryContents[index]; //Cast to instruction required; stored as type Data
+			return instr;
+		}		
 		return null;
 	}
 
@@ -23,9 +26,10 @@ public class MemoryModule implements MainMemory {
 		return 0;
 	}
 	
-	public void writeInstruction(Instruction instr, int index) { //unclear if index should be explicitly specified, or done using 
-																//pointer field
+	public void writeInstruction(Instruction instr, int index) { //index will be specified by destination field in TransferInstr
 		
 	}
+	
+	public void writeInteger
 
 }
