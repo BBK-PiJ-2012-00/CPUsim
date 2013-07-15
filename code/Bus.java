51951c083ae7b56967a780e28f2802d3f3e1a9df
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-15 14:25:51 +0100

    Added Bus interface for System Bus class, which will encapsulate bus lines.

---
+++ code/Bus.java
@@ -0,0 +1,10 @@
+package code;
+
+/*
+ * An interface for the System Bus of the simulator. It seems wise to encapsulate the (initially) three bus lines 
+ * in a System Bus class, meaning that lines must be accessed via the System Bus class and not directly, ensuring
+ * a higher degree of data integrity.
+ */
+public interface Bus {
+
+}
