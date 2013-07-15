a1073e4c5fa42b57cba9075fef2a8114b835b9d0
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-15 14:07:30 +0100

    Initial code done. More to follow later.

--- code/DataLineImpl.java
+++ code/DataLineImpl.java
@@ -1,15 +1,15 @@
 package code;
 
 public class DataLineImpl implements DataLine {
-	private Data data; //DataLine will carry data of type Instruction and int at the very least; Data required as supertype.
+	private Data data;
 
 	@Override
-	public void put(int value) {
+	public void put(Data value) {
 		data = value;
 	}
 
 	@Override
-	public Data read() { //Integers aren't the only data type to traverse data line
+	public Data read() {
 		return data;
 	}
 
