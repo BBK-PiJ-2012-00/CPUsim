23c1301d3a389dfb8ee2c2eb19306dbbecb2ceaf
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-15 14:15:18 +0100

    AddressLine is now an interface for the address line of the System Bus.

--- code/AddressLine.java
+++ code/AddressLine.java
@@ -1,15 +1,32 @@
 package code;
 
-public class AddressLine implements BusLine {
-	private int address;
+/*
+ * An interface for the address line component of the System Bus. The address line holds an address value
+ * as an integer when the address is a location in main memory. The address line holds no value when the 
+ * System Bus is making a transfer from main memory to the CPU, as all data is delivered into the Memory Buffer
+ * Register.
+ */
+public interface AddressLine {
 	
-	public void put(int address) {
-		//Check address validity
-		this.address = address;
-	}
+	/*
+	 * A method to put a main memory address location on the address line.
+	 * 
+	 * @param int address the memory address destination of the bus transfer.
+	 */
+	public void put(int address);
 	
-	public int read() {
-		return this.address;
-	}
+	/*
+	 * A method used to signify a transfer from memory back to MBR of CPU. 
+	 * May not be required long term. Sets address to -1, which is not a valid
+	 * main memory address location. 
+	 */
+	public void put();
+	
+	/*
+	 * A method for reading an address from the address line of the bus.
+	 * 
+	 * @return int the address on the address line.
+	 */
+	public int read();		
 
 }
