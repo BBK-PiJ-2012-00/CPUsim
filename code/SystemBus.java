76a4b89e0692c49386e653b66fc061951b8de11f
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-15 14:37:02 +0100

    SystemBus implements the Singleton pattern to ensure only one instance, removing the potential for inadvertent duplicates which would result in transfer issues between CPU and memory.

--- code/SystemBus.java
+++ code/SystemBus.java
@@ -1,7 +1,12 @@
 package code;
 
+/*
+ * There can only be one System Bus module, but the System Bus can have more than one of each line;
+ * i.e. two ControlLines and corresponding Data/AddressLines for performance. These can be added as
+ * fields as and when necessary.
+ */
 public class SystemBus implements Bus {
-	private static SystemBus sysBusInstance = null; //System Bus is a singleton.
+	private static SystemBus systemBus = null; //System Bus is a singleton. Should perhaps be referenced as type Bus.
 	
 	private ControlLine controlLine;
 	private DataLine dataLine;
@@ -9,13 +14,21 @@ public class SystemBus implements Bus {
 	
 	private SystemBus() {
 		controlLine = new ControlLineImpl();
-		
+		dataLine = new DataLineImpl();
+		addressLine = new AddressLineImpl();
 	}
 	
 	/*
-	 * There can only be one System Bus module, but the System Bus can have more than one of each line;
-	 * i.e. two ControlLines and corresponding Data/AddressLines for performance. These can be added as
-	 * fields as and when necessary.
+	 * Singleton global access point.
 	 */
+	public synchronized static SystemBus getInstance() { //Synchronized to ensure no concurrency issues with pipelining
+		if (systemBus == null) {
+			systemBus = new SystemBus();
+			return systemBus;
+		}
+		return systemBus;
+	}
+	
+	
 
 }
