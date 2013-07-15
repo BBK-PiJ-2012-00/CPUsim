c7e3dcf17037c08750af98a83a7b99f7c298260a
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-15 15:43:17 +0100

    SystemBus references now only control lines, with control lines referencing the address/data lines they coordinate.

--- code/SystemBus.java
+++ code/SystemBus.java
@@ -3,19 +3,21 @@ package code;
 /*
  * There can only be one System Bus module, but the System Bus can have more than one of each line;
  * i.e. two ControlLines and corresponding Data/AddressLines for performance. These can be added as
- * fields as and when necessary.
+ * fields as and when necessary.  Address and data lines are accessed via their corresponding control line,
+ * so that in the event of multiple lines, access is simplified and integrity is protected.
+ * 
+ * Should the SystemBus class reference only the control lines, which then handle activation of data/address lines?
+ * Closer to reality.
  */
 public class SystemBus implements Bus {
 	private static SystemBus systemBus = null; //System Bus is a singleton. Should perhaps be referenced as type Bus.
 	
 	private ControlLine controlLine;
-	private DataLine dataLine;
-	private AddressLine addressLine;
+	
+	//References to CPU (MAR, MBR) and main memory
 	
 	private SystemBus() {
 		controlLine = new ControlLineImpl();
-		dataLine = new DataLineImpl();
-		addressLine = new AddressLineImpl();
 	}
 	
 	/*
