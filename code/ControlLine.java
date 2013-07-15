38f9dd7425ff1aea9144c8da9a059539db582c90
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-15 14:21:35 +0100

    Like with the other bus lines, ControlLine is now an interface; ControlLineImpl the concrete Implementation. Part of bus restructuring.

--- code/ControlLine.java
+++ code/ControlLine.java
@@ -1,70 +1,53 @@
 package code;
 
-public class ControlLine implements BusControlLine {
-	private static ControlLine instance = null; //Reference to the one instance of this class
-	private BusLine addrLine;
-	private BusLine dataLine;
-	public boolean inUse; //To ensure a write is followed by a read operation.
-	
-	//References to CPU and memory
-	
-	
+public interface ControlLine {
 	/*
-	 * The program must have one and only one System Bus so that all modules reference the exact same bus line objects.
-	 * (Singleton) 
-	 */
-	private ControlLine() {
-		addrLine = new AddressLine();
-		dataLine = new DataLine();
-	}
-	
-	public static ControlLine getInstance() { //May need to be synchronized depending on concurrency
-		if (instance == null) {
-			instance = new ControlLine();
-		}
-		return instance;
-	}
-	
-	/*
-	 * Should this method also invoke the memory to read the bus? A write to the bus by the CPU should be
-	 * followed by memory reading from the bus, and vice versa. 
+	 * Synchronous, clock-based coordination of the Bus would result in a more complicated than necessary design, so
+	 * it makes sense to adopt an asynchronous, handshaking-protocol method of sharing the Bus between the CPU and memory.
+	 * Using the Observer pattern would again introduce more complexity; proposed is the use of methods such as memoryWrite / 
+	 * memoryRead. Memory is idle, waiting
+	 * for requests from the processor.  The processor requests a read and then waits for data to arrive.
 	 * 
-	 *  CPUissue/memoryIssue booleans; both cannot be true or false at same time. Simplifies bus; two seperate
-	 *  methods would require more coordination (synchronization) issues. 
-	 * (non-Javadoc)
-	 * @see code.BusControlLine#writeToBus(int, int)
+	 * Processor issues read -> memory locates address and places contents on bus -> sends back to processor: processor
+	 * is idle waiting for the results of the read.  Memory invokes a sendToCPU() method to signal returning data. 
 	 */
-	synchronized public boolean writeToBus(boolean cpuIssue, boolean memoryIssue, int addr, int data) { //An atomic operation, to preserve data integrity
-		if (!inUse) {
-			if (cpuIssue) { //this means CPU has issued the write and memory should read from the bus
-				inUse = true;
-				addrLine.put(addr);//The memory address where the data is to be stored
-				dataLine.put(data);
-				//call method on memory to read from bus?
-			}
-			inUse = true;
-			addrLine.put(addr);
-			dataLine.put(data);
-			return true;
-		}
-		return false;
-	}
-	
-	
-	
-	public int readAddressLine() {
-		return addrLine.read();
-	}
-	
-	
-	public int readDataLine() {
-		return dataLine.read();
-	}
-	
-	public boolean isInUse() {
-		return inUse;
-	}
 
+	public interface BusControlLine {
+		
+		
+		/*
+		 * To enforce data integrity during pipelined operation, the use of the bus is restricted to ensure write operations 
+		 * are followed y read operations by the receiver module.  Setting the address and data lines must be an
+		 * atomic operation for this reason.  If the bus is not already in use the method will return
+		 * true to reflect a successful write, otherwise it returns false.  
+		 * 
+		 * @param int addr the address to be written to the address line.
+		 * @param int data the data to be written to the data line.
+		 * @return boolean if the write is successful, returns true; otherwise, false.
+		 */
+		public boolean writeToBus(int addr, int data); //This may not work -> addr and data come from MAR/MBR: separate objects
+		//This is where a port might come in -> MBR/MAR pass data to the port seperately, which then interacts with bus
+		
+		public int readAddressLine();
+		
+		public int readDataLine();
+		
+		/*
+		 * This pertains to a boolean inUse field in the ControlLine class; it is important to control the use of the
+		 * System Bus lines.  When one module has written to the bus lines, the information must be read by the other module
+		 * before anything subsequent can be written to the bus line.  The boolean flag should facilitate this.
+		 */
+		public boolean isInUse();
+
+		/*
+		 * Think about concurrency! Bus is an object used to pass data and addresses - there should be multiple address/data
+		 * lines, and corresponding control lines to allow pipelined access.
+		 * Race condition problem:
+		 * There must also be a way of ensuring that when one module has placed something on the bus, the other module must
+		 * perform a read before the bus can be written to again... so there should perhaps be an inUse boolean flag to control
+		 * this.
+		 * What gets written must be read before anyone can write to the bus again.
+		 */
 	
 	
 	
---
+++ code/ControlLineImpl.java
@@ -0,0 +1,68 @@
+package code;
+
+public class ControlLineImpl {
+	private static ControlLine instance = null; //Reference to the one instance of this class
+	private BusLine addrLine;
+	private BusLine dataLine;
+	public boolean inUse; //To ensure a write is followed by a read operation.
+	
+	//References to CPU and memory
+	
+	
+	/*
+	 * The program must have one and only one System Bus so that all modules reference the exact same bus line objects.
+	 * (Singleton) 
+	 */
+	private ControlLineImpl() {
+		addrLine = new AddressLine();
+		dataLine = new DataLine();
+	}
+	
+	public static ControlLine getInstance() { //May need to be synchronized depending on concurrency
+		if (instance == null) {
+			instance = new ControlLineImpl();
+		}
+		return instance;
+	}
+	
+	/*
+	 * Should this method also invoke the memory to read the bus? A write to the bus by the CPU should be
+	 * followed by memory reading from the bus, and vice versa. 
+	 * 
+	 *  CPUissue/memoryIssue booleans; both cannot be true or false at same time. Simplifies bus; two seperate
+	 *  methods would require more coordination (synchronization) issues. 
+	 * (non-Javadoc)
+	 * @see code.BusControlLine#writeToBus(int, int)
+	 */
+	synchronized public boolean writeToBus(boolean cpuIssue, boolean memoryIssue, int addr, int data) { //An atomic operation, to preserve data integrity
+		if (!inUse) {
+			if (cpuIssue) { //this means CPU has issued the write and memory should read from the bus
+				inUse = true;
+				addrLine.put(addr);//The memory address where the data is to be stored
+				dataLine.put(data);
+				//call method on memory to read from bus?
+			}
+			inUse = true;
+			addrLine.put(addr);
+			dataLine.put(data);
+			return true;
+		}
+		return false;
+	}
+	
+	
+	
+	public int readAddressLine() {
+		return addrLine.read();
+	}
+	
+	
+	public int readDataLine() {
+		return dataLine.read();
+	}
+	
+	public boolean isInUse() {
+		return inUse;
+	}
+
+}
