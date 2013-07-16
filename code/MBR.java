ad6122ea2ceb6edde9c3229a231095190015e059
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-16 17:23:30 +0100

    Updated MBR class to prepare for change in memory read logic.

--- code/MBR.java
+++ code/MBR.java
@@ -3,9 +3,10 @@ package code;
 public class MBR {
 	private Data registerContents;
 	
-	public void write(Data data) {
+	public boolean write(Data data) { //Successful write returns true
 		//Size is restricted in Instruction/Operand classes
 		registerContents = data;
+		return true;
 	}
 	
 	public Data read() { //May need to employ readAsInt(), readAsInstr() etc, if necessary
