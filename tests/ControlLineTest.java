ebb0e5ab494614af3e4726eb3f59c2f4a9c57100
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-16 12:58:59 +0100

    Beginning testing of bus lines.

---
+++ tests/ControlLineTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import code.ControlLine;
+import code.ControlLineImpl;
+import code.AddressLine;
+import code.AddressLineImpl;
+import code.DataLine;
+import code.DataLineImpl;
+
+import org.junit.Test;
+
+public class ControlLineTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ tests/DataLineTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import code.DataLine;
+import code.DataLineImpl;
+
+public class DataLineTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
