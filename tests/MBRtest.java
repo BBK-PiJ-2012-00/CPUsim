b514201473b34b297cef21d940338028227b4b07
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-16 17:36:00 +0100

    Initial testing of MBR complete and successful.

--- tests/MBRtest.java
+++ tests/MBRtest.java
@@ -2,13 +2,33 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import code.*;
+
 public class MBRtest {
+	private MBR mbr;
+	private Instruction testInstr;
+	
+	@Before
+	public void setUp() {
+		mbr = new MBR();
+		testInstr = new TransferInstr(Opcode.MOVE, 0, 0);
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testWrite() {
+		assertTrue(mbr.write(testInstr)); //Successful write should return true.		
 	}
+	
+	@Test
+	public void testRead() {
+		mbr.write(testInstr);
+		Data expected = testInstr;
+		Data output = mbr.read();
+		assertEquals(expected, output);		
+	}
+
 
 }
