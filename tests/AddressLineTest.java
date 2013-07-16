1aa68424156c53e3a2298d98eb2695f0dbc70a4c
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-16 15:27:51 +0100

    Starting AddressLine tests.

---
+++ tests/AddressLineTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import code.AddressLineImpl;
+import code.AddressLine;
+
+public class AddressLineTest {
+	AddressLine aLine;
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+//	@Override
+//	public void put(int address) {
+//		//Check address validity?
+//		this.address = address;
+//	}
+//	
+//	@Override
+//	public void put() {
+//		this.address = -1; //-1 not a main memory address; used for transfers to MBR of CPU.
+//	}
+//	
+//	@Override
+//	public int read() {
+//		return this.address;
+//	}	
+
+}
