55d0ba16f58932d018fde876d21256f87620dc2a
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-17 14:00:05 +0100

    Updated comments of DataLine class pending change to DataBus.

--- code/DataLine.java
+++ code/DataLine.java
@@ -1,6 +1,13 @@
 package code;
 
 /*
+ * 
+ * DataLine pending change to DataBus; typically, a data line in a parallel channel carries one bit,
+ * and a data bus width of 32 lines is used to transmit a 32-bit word. Collectively these lines are called
+ * a data bus.  DataBus would more accurately describe the function of this class; the DataBus class
+ * can be interpreted as containing 32 data lines, enabling it to carry 32-bit words of data.  Thus, the data
+ * field of this class representing the contents of the data bus is limited to a maximum value of 2^32.
+ * 
  * An interface to represent a System Bus data line. Data lines carry data of type Data between main memory and 
  * the CPU. Type Data is required because data lines will carry both Instructions as well as other formats, such as
  * integers and possibly floating point in the future.
