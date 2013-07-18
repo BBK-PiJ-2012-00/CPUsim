8f6775b0569a04b45eea75cf7971a8826a2ece34
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-18 23:27:21 +0100

    Completed testing of MAR class as it currently stands. Methods may be added or changed in the future, in which case relevants tests will be done.

--- tests/MARtest.java
+++ tests/MARtest.java
@@ -17,8 +17,18 @@ public class MARtest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRead() {
+		int output = mar.read();
+		int expected = 0; //Default value of registerContents field is 0.
+		assertEquals(expected, output);
+	}
+	
+	@Test
+	public void testWrite() {
+		mar.write(99);
+		int expected = 99;
+		int output = mar.read();
+		assertEquals(expected, output);
 	}
 
 }
