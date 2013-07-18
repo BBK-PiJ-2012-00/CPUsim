782e040247f340dc306dd1ee6076dbce9efdea0e
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-18 17:23:29 +0100

    About to start testing SystemBusController.

---
+++ tests/SystemBusControllerTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SystemBusControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
