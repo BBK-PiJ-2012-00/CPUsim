96b1bc52c263737d31709095d136010ef7ba4467
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-18 23:24:26 +0100

    Added MAR test.

---
+++ tests/MARtest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import code.MemoryAddressRegister;
+import code.MAR;
+
+public class MARtest {
+	private MemoryAddressRegister mar;
+	
+	@Before
+	public void setUp() {
+		mar = new MAR();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
