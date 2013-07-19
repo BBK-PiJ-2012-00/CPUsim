74c4e860f91521e3d9d8b3cd5f6f6fba492a064f
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-19 19:05:27 +0100

    Re-tested ControlLine following updates to it to encapsulate an entire read operation (starting from MAR). All tests fine, although it should be noted that it is not feasible to create a separate ControlLine object, even for testing, as this ends up creating two ControlLine objects, as one is automatically created when the SystemBusController is used for the first time; duplicates of the same object in this case causes data integrity problems.

--- tests/ControlLineTest.java
+++ tests/ControlLineTest.java
@@ -4,15 +4,28 @@ import static org.junit.Assert.*;
 
 import code.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /*
  * Needs testing with multiple threads for pipelining.
  */
 public class ControlLineTest {
-	private ControlLine cLine = new ControlLineImpl();
-	private Instruction testInstr = new TransferInstr(Opcode.STORE, 0, 0);
-	private MainMemory testMemory = MemoryModule.getInstance();
+	private ControlLine cLine;
+	private Instruction testInstr;
+	private MainMemory testMemory;
+	private BusController busController; 
+	
+	@Before
+	public void setUp() {
+		testInstr = new TransferInstr(Opcode.STORE, 0, 0);
+		testMemory = MemoryModule.getInstance();
+		busController = SystemBusController.getInstance();
+		cLine = ((SystemBusController) busController).accessControlLine(); //This solves problem of duplicate MBR modules being
+		//created with the creation of more than one ControlLine; MBR is created upon creation of ControlLine, which would be created
+		//twice if instantiated in its own right in this test class; therefore, the solution is to access the same ControlLine
+		//object through the busController, which is required during these tests.
+	}
 
 	@Test
 	public void testInitialisation() {
@@ -35,17 +48,37 @@ public class ControlLineTest {
 	
 	@Test
 	public void deliverToMemoryTest() {
-		assertFalse(cLine.deliverToMemory()); //Should return false as no address or data has been specified
+		assertFalse(cLine.deliverToMemory(true)); //Should return false as no address or data has been specified
 		//A true return value is tested as part of writeToBus tests below
 	}
 	
+	/*
+	 * Tests a complete memory read, both the first and second stages; during the first stage of a read the address 
+	 * location that holds the value to be read is delivered to main memory from the MAR via the control line. The second
+	 * stage encompasses the value held at the address location specified by the address line being loaded onto the data
+	 * line and being delivered to the MBR.
+	 */
 	@Test
 	public void testWriteToBus_MemoryReadOperation() {
-		assertTrue(cLine.writeToBus(-1, testInstr)); //-1 to signify memory read, with delivery to CPU(MBR)		
+		assertTrue(cLine.writeToBus(-1, testInstr)); //-1 to signify memory read, with delivery to CPU(MBR)
 	}
 	
+	/*
+	 * Will not work when test method accessMockMBR() of ControlLine is commented out/deleted after completion
+	 * of testing.
+	 */
 	@Test
-	public void testWriteToBus_MemoryWriteOperatoin() {
+	public void testWriteToBus_MemoryReadOperation2() { //Tests contents are actually delivered to MBR
+		cLine.writeToBus(10, testInstr); //Writes a value to memory
+		cLine.writeToBus(10, null); //Issues a read (as Data value is null)
+		Data expected = testInstr;
+		Data output = ((ControlLineImpl) cLine).accessMockMBR();
+		assertEquals(expected, output);
+	}
+	
+	
+	@Test
+	public void testWriteToBus_MemoryWriteOperation() {
 		assertTrue(cLine.writeToBus(0, testInstr)); //0 is a valid memory address
 	}
 	
@@ -56,6 +89,7 @@ public class ControlLineTest {
 		assertEquals(expected, output);
 		//Checks contents of memory address is loaded with correct instruction when loaded via system bus
 	}
+
 	
 	
 	//If MBR is a singleton, its contents can be checked for read operation
