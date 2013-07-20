4284892f0d4823620a8add8a7daff1a1cbcd49bc
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-20 17:58:37 +0100

    Drafting out some rough code for ControlUnit operation (which ought to shed light on any holes elsewhere, i.e. system bus operation). Likely to be revised and refactored.

--- code/ControlUnitImpl.java
+++ code/ControlUnitImpl.java
@@ -1,14 +1,46 @@
 package code;
 
 public class ControlUnitImpl implements ControlUnit {
-private BusController systemBus = SystemBusController.getInstance();
+	private BusController systemBus = SystemBusController.getInstance();
 	
-	private MBR mbr = new MBR(); //Reference these with their corresponding interfaces
-	private MAR mar = new MAR();
+	private ProgramCounter pc;
 	
-	private RegisterFile genRegisters = new RegisterFile16();
+	private IR ir;
 	
-	//Stages represented by methods
+	private MemoryBufferRegister mbr;
+	private MemoryAddressRegister mar;
+	
+	private RegisterFile genRegisters;
+	
+	public ControlUnitImpl() {
+		systemBus = SystemBusController.getInstance();
+		mbr = MBR.getInstance();
+		mar = MAR.getInstance();
+		genRegisters = new RegisterFile16();
+		pc = new PC();
+		ir = new IR();
+	}
+	
+	public void go() {
+		InstructionCycleStage fetchStage = new FetchDecodeStage();
+
+	}
+	
+	public class FetchDecodeStage implements InstructionCycleStage {
+		
+		public void run() {
+			this.instructionFetch();			
+		}
+		
+		
+		private void instructionFetch() {
+			mar.write(pc.getValue());
+		}
+		
+	}
+	
+	//Stages represented by methods --> these methods should be embedded within objects of type Stage
+	//This allows for pipelining to be more easily implemented later.
 	
 	public void instructionFetch() {
 		//Code for instruction fetch stage
