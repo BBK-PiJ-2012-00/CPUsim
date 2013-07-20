c484419c5a70ceee4a75a28e386e70b0690d3d33
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-20 16:12:51 +0100

    MAR, like MBR and for the same reasons, is now a singleton with a global access method getInstance().

--- code/MAR.java
+++ code/MAR.java
@@ -1,10 +1,26 @@
 package code;
 
 /*
- * Class to represent Memory Address Register.
+ * Class to represent Memory Address Register. This is a singleton to ensure only one instance.  See MBR
+ * class for motivation.
  */
 public class MAR implements MemoryAddressRegister {
+	private static MemoryAddressRegister mar;
+	
 	private int registerContents;
+	
+	private MAR() {
+		super();
+		//Constructor only made explicit so that it can be made private, enforcing singleton.
+	}
+	
+	public synchronized static MemoryAddressRegister getInstance() {
+		if (mar == null) {
+			mar = new MAR();
+			return mar;
+		}
+		return mar;
+	}
 	
 	/*
 	 * 	 * (non-Javadoc)
