665a8681e6755d8f273c462f3bf77a6320c23dc9
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-20 16:38:13 +0100

    Added some potential adjustments to bus implementation to comments of ControlLine.

--- code/ControlLineImpl.java
+++ code/ControlLineImpl.java
@@ -1,5 +1,22 @@
 package code;
 
+/*
+ * Would it be more realistic to have memory write operations physically take the Data from the
+ * MBR as opposed to it being passed to the dataLine via the SystemBusController? Should data / address lines
+ * have references to MBR/MAR respectively?
+ */
+
+/*
+ * The passing of address/data values to ControlLine needs attention; either they all come
+ * via the BusController, or the ControlLine is prompted to examine the MAR/MBR registers 
+ * and act accordingly; BusController should in this case only be responsible for initiating
+ * action (and, crucially, ensuring atomic operation of ControlLine; that one operation completes
+ * before the next may begin.  Also, in the event of multiple control lines to facilitate performance,
+ * the BusController is responsible for managing access to lines (for this to work, safeguards will have to
+ * be in place to coordinate access to MAR/MBR).  
+ * ControlLine would thus require direct access to MAR/MBR to obtain the values.
+ */
+
 public class ControlLineImpl implements ControlLine {
 	private AddressLine addressLine;
 	private DataLine dataLine;
@@ -8,6 +25,7 @@ public class ControlLineImpl implements ControlLine {
 	private MainMemory memory;
 	
 	private MemoryBufferRegister mbr;//Reference to CPU's MBR
+	private MemoryAddressRegister mar; //Reference to CPU's MAR
 	
 	
 	public ControlLineImpl() {
@@ -15,6 +33,7 @@ public class ControlLineImpl implements ControlLine {
 		dataLine = new DataLineImpl();
 		memory = MemoryModule.getInstance();
 		mbr = MBR.getInstance();
+		mar = MAR.getInstance();
 	}
 	
 	/* 
@@ -61,5 +80,27 @@ public class ControlLineImpl implements ControlLine {
 		}
 		return memory.notify(addressLine.read(), dataLine.read());
 	}
+	
+	
+	
+	//This is if parameters are passed through BusController; may be reinstated if concurrency causes issues.
+//	synchronized public boolean writeToBus(int address, Data data) { //This method now is responsibly only for writing to bus, with accessing lines
+//		//delegated to deliverTo...() methods below. Better encapsulation and separation of concerns, as well as better use of bus lines.
+//		if (address == -1) { //Indicates transfer from memory to CPU (2nd phase of memory read; delivery from memory to MBR)
+//			//no need to place address on address bus -> if address field in AddressLine is null/0, this signifies delivery
+//			//to CPU as opposed to memory (and is true to reality).
+//			dataLine.put(data); //This is functionally redundant, but will be useful for GUI animation of bus lines
+//			return this.deliverToMBR(); //Complete read operation. 
+//		}
+//		else if (data == null) { //Signifies first phase of a read; MAR places address on address line, prompting memory to
+//			//place contents of the address on the address line onto data line for return to MBR.
+//			addressLine.put(address);
+//			return this.deliverToMemory(true);
+//		}
+//		//Memory write code:
+//		addressLine.put(address);
+//		dataLine.put(data);
+//		return this.deliverToMemory(false);	//False -> not a read operation (write operation)	
+//	}
 
 }
