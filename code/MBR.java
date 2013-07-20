b4e6aeaef3b4d4bd5a6d1e83313490306681718a
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-20 15:52:27 +0100

    As a safety feature, MBR is now a singleton; duplicate MBR objects are problematic, and there should only ever be one. This also solves the problem of passing reference to the one MBR to all the relevant classes that require a reference to it.

--- code/MBR.java
+++ code/MBR.java
@@ -1,7 +1,34 @@
 package code;
 
+/*
+ * This class is now a singleton (there can only be once instance, with a global access point). Because this class
+ * is referenced by the ControlLine class, as well as the ControlUnit class, it is important that only one instance
+ * of this class can exist (it has already occurred during testing that two separate instances co-existed, causing 
+ * data inegrity issues). As the ControlLine is accessible only via the BusController, it would mean that the MBR
+ * would have to be instantiated in the ControlUnit first, and then passed as a constructor parameter to the BusController,
+ * and then to the ControlUnit class.  As the BusController has no need for an MBR reference, this causes added complication.
+ * Thus, a singleton would allow both the ControlUnit and the ControlLine access, without worrying about duplicate objects
+ * and passing objects through classes which need not have any knowledge of the MBR.  The same goes for the MAR class.
+ */
+
 public class MBR implements MemoryBufferRegister {
+	private MemoryBufferRegister MBRinstance;
+	
 	private Data registerContents;
+	
+	private MBR() {
+		//Constructor only explicitly referenced in order to make it private, preventing instantiation
+		//from outside the class.
+		super(); //Calls default Object constructor.
+	}
+	
+	public synchronized MemoryBufferRegister getInstance() {
+		if (this.MBRinstance == null) {
+			this.MBRinstance = new MBR();
+			return this.MBRinstance;
+		}
+		return MBRinstance;
+	}
 	
 	@Override
 	public boolean write(Data data) { //Successful write returns true
