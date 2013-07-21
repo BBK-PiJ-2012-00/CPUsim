1e380ea48c514bc5bdd074abdc93e3b688ba6d58
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-21 18:14:17 +0100

    RegisterFile16 testing complete so far; all successful. If more methods are added, these will be tested at that time.

--- tests/RegisterFile16Test.java
+++ tests/RegisterFile16Test.java
@@ -6,32 +6,46 @@ import org.junit.Before;
 import org.junit.Test;
 
 import code.Data;
+import code.Operand;
+import code.OperandImpl;
+import code.RegisterFile;
+import code.RegisterFile16;
 
 public class RegisterFile16Test {
+	RegisterFile registers;
+	Operand testOperand;
+	
 
 	@Before
 	public void setUp() throws Exception {
+		registers = new RegisterFile16();
+		testOperand = new OperandImpl(10);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRead() {
+		assertNull(registers.read(0)); //Should be null as no data held in registers
 	}
 	
-//	@Override
-//	public void write(int index, Data data) {
-//		if (index > -1 && index < 16) { //Ensure valid index (0-15)
-//			generalPurposeRegisters[index] = data;
-//		}
-//		//Otherwise do nothing (throw exception?)
-//	}
-//	@Override
-//	public Data read(int index) {
-//		if (index < -1 && index < 16) { //Ensure valid index (0-15)
-//			return generalPurposeRegisters[index];
-//		}
-//		//Throw exception?
-//		return null;
-//	}
+	@Test
+	public void testInvalidRead() { //Testing the attempted read of an out of bounds register index
+		assertNull(registers.read(16));
+	}
+	
+	@Test
+	public void testWrite() {
+		registers.write(15, testOperand);
+		Data output = registers.read(15);
+		Data expected = testOperand;
+		assertEquals(expected, output);
+	}
+	
+	@Test 
+	public void testInvalidWrite() { //Writing to an invalid index should have no effect
+		registers.write(16, testOperand);
+		for (int i = 0; i < 16; i++) {
+			assertNull(registers.read(i)); //Check that no registers have been wrongly updated in event of false index
+		}
+	}
 
 }
