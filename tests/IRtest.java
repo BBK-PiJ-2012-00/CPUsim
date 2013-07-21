fd346cf3d743fe17022453e5cb2ecc9a19bd5f6b
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-21 13:54:02 +0100

    IR testsed successfully. If more methods are added, these will be tested then.

--- tests/IRtest.java
+++ tests/IRtest.java
@@ -2,14 +2,37 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import code.InstructionRegister;
+import code.IR;
+import code.Instruction;
+import code.TransferInstr;
+import code.Opcode;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class IRtest {
+	private InstructionRegister ir;
+	private Instruction testInstr;
+	
+	@Before
+	public void setUp() {
+		ir = new IR();
+		testInstr = new TransferInstr(Opcode.STORE, 0, 0);
+	}	
 	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRead() {
+		assertNull(ir.read()); //read() should return null because contents field is uninitialised.
+	}
+	
+	@Test
+	public void testLoadIR() {
+		ir.loadIR(testInstr);
+		Instruction output = ir.read();
+		Instruction expected = testInstr;
+		assertEquals(expected, output);
 	}
 
 }
