52a6f05af2fa519f9ad52dcee7e1db7f0000bcbe
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-21 13:59:49 +0100

    Tested getValue() successfully.

--- tests/PCtest.java
+++ tests/PCtest.java
@@ -5,15 +5,33 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import code.ProgramCounter;
+import code.PC;
+
 public class PCtest {
+	private ProgramCounter pc;
 
 	@Before
 	public void setUp() throws Exception {
+		pc = new PC();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetValue() {
+		int output = pc.getValue();
+		int expected = 0;
+		assertEquals(expected, output); //Default address value is 0, therefore 0 should be returned
 	}
+	
+	
+	
+	
+//	public void incrementPC() {
+//		nextInstructionPointer++;
+//	}
+//	
+//	public void setPC(int address) { //Used to set PC pointer
+//		this.nextInstructionPointer = address;
+//	}
 
 }
