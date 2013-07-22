4e800763e87c3d00e1768d380359701a2f40b3b8
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-22 17:59:11 +0100

    Written out major methods of ALU for addition, subtraction, division and multiplication.

--- code/ALU.java
+++ code/ALU.java
@@ -6,7 +6,11 @@ package code;
  */
 public class ALU {
 	
-	public static Operand AdditionUnit1(Operand operand1, Operand operand2) {
+	private ALU() { //To prevent instantiation; all methods are static (as ALU is like a utility class)
+		super();
+	}
+	
+	public static Operand AdditionUnit(Operand operand1, Operand operand2) {
 		int op1 = operand1.unwrapInteger();
 		int op2 = operand2.unwrapInteger();
 		int sum = op1 + op2;
@@ -17,5 +21,42 @@ public class ALU {
 		Operand result = new OperandImpl(sum);
 		return result;
 	}
+	
+	public static Operand SubtractionUnit(Operand operand1, Operand operand2) {
+		int op1 = operand1.unwrapInteger();
+		int op2 = operand2.unwrapInteger();
+		int intResult = op1 - op2;
+		if (intResult > 2147483647 || intResult < -2147483648) { //Max/min signed integer for 32-bit machine -> would cause overflow
+			//Throw exception
+			return null;
+		}
+		Operand result = new OperandImpl(intResult);
+		return result;
+	}
+	
+	public static Operand DivisionUnit(Operand operand1, Operand operand2) {
+		int op1 = operand1.unwrapInteger();
+		int op2 = operand2.unwrapInteger();
+		int intResult = op1 / op2;
+		if (intResult > 2147483647 || intResult < -2147483648) { //Max/min signed integer for 32-bit machine -> would cause overflow
+			//Throw exception
+			return null;
+		}
+		Operand result = new OperandImpl(intResult);
+		return result;
+	}
+	
+	public static Operand MultiplicationUnit(Operand operand1, Operand operand2) {
+		int op1 = operand1.unwrapInteger();
+		int op2 = operand2.unwrapInteger();
+		int intResult = op1 * op2;
+		if (intResult > 2147483647 || intResult < -2147483648) { //Max/min signed integer for 32-bit machine -> would cause overflow
+			//Throw exception
+			return null;
+		}
+		Operand result = new OperandImpl(intResult);
+		return result;
+	}
+
 
 }
