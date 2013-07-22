bfd50cc05831d8fbea970f15a09808f55d217d7b
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-22 17:53:20 +0100

    Drafted out addition unit for ALU.

--- code/ALU.java
+++ code/ALU.java
@@ -6,8 +6,16 @@ package code;
  */
 public class ALU {
 	
-	public static Operand useAdditionUnit(Operand operand) {
-		return null;
+	public static Operand AdditionUnit1(Operand operand1, Operand operand2) {
+		int op1 = operand1.unwrapInteger();
+		int op2 = operand2.unwrapInteger();
+		int sum = op1 + op2;
+		if (sum > 2147483647 || sum < -2147483648) { //Max/min signed integer for 32-bit machine -> would cause overflow
+			//Throw exception
+			return null;
+		}
+		Operand result = new OperandImpl(sum);
+		return result;
 	}
 
 }
