065ea92378fd32fe5fbcab0b1b021b877902375c
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-24 16:50:34 +0100

    Updated method signatures. Register is an interface for user visible registers (of which there is only likely to be the one).

--- code/Register.java
+++ code/Register.java
@@ -1,25 +1,10 @@
 package code;
 
-public interface Register {
+public interface Register {		
 	
+	public Operand read();
 	
-	
-	//For methods common to all control registers; may not be the case that they have much in common!
-	
-	/*
-	 * MBR needs to hold type Data; it holds all data types that come into CPU from memory, and 
-	 * 	data that goes out to memory.
-	 * MAR holds integers only; address indexes.
-	 * 
-	 * PC has an increment method, set, read.
-	 * 
-	 * General purpose registers will be a register file of 16 registers to begin with
-	 * MBR as a register file? Or just the one slot? One slot would be simpler.
-	 */
-	
-	public int read();
-	
-	public int write(Data data);
+	public void write(Operand operand);
 	
 	
 
