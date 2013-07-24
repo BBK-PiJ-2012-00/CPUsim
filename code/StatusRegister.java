77cea6ebdd7e31bbd03f0764b566d9243dddc14d
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-24 16:59:40 +0100

    Updated comments.

--- code/StatusRegister.java
+++ code/StatusRegister.java
@@ -3,6 +3,9 @@ package code;
 /*
  * A class to represent a status register, generally for use with conditional branch instructions. User-visible,
  * can be referenced as r16 in assembly code.
+ * 
+ * This register may automatically be set as the result of certain operations? Easier to set it manually with explicit
+ * LOAD instruction.  Can be incremented/decremented using arithmetic operations.
  */
 public class StatusRegister implements Register {
 	private Operand contents;
