b2c457c70860c795c40790008f31d0e846fdee17
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-29 15:07:23 +0100

    For standard execution.

---
+++ code/StandardExecuteStage.java
@@ -0,0 +1,17 @@
+package code;
+
+public class StandardExecuteStage extends ExecuteStage {
+
+	public StandardExecuteStage(InstructionRegister ir, ProgramCounter pc,
+			RegisterFile genRegisters, Register statusRegister,
+			WriteBackStage writeBackStage) {
+		super(ir, pc, genRegisters, statusRegister, writeBackStage);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void forward(Operand result) {
+		this.getWriteBackStage().receive(result);
+	}
+
+}
