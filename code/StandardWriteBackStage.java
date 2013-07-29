7d36783ff44331c413e1b710dfbb8149aed2f85b
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-29 15:10:51 +0100

    For non pipelined execution.

---
+++ code/StandardWriteBackStage.java
@@ -0,0 +1,15 @@
+package code;
+
+public class StandardWriteBackStage extends WriteBackStage {
+
+	public StandardWriteBackStage(InstructionRegister ir, RegisterFile genRegisters) {
+		super(ir, genRegisters);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void receive(Operand result) {
+		this.instructionWriteBack(result);
+	}
+
+}
