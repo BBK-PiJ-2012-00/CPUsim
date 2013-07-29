eb5941d40614ebbcc3d958414c193c69ff8aebd9
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-29 15:20:32 +0100

    Removed instructionFetch(), decode(), execute() and writeBack() methods; these are now embedded into their respective Stage classes. This makes for a clearer control unit class, and execution is split neatly into stages.

--- code/ControlUnit.java
+++ code/ControlUnit.java
@@ -7,16 +7,17 @@ package code;
 public interface ControlUnit {
 	
 	
-	//Stages represented by methods
+	//Stages of the instruction cycle are now represented by the Stage classes, removing the need for these
+	//methods to be present in the control unit itself.
 	
-	public void instructionFetch(); 
+	//public void instructionFetch(); 
 		//Code for instruction fetch stage
 	
-	public int instructionDecode(); //Returns opcode value (numeric)
+	//public int instructionDecode(); //Returns opcode value (numeric)
 	
-	public void instructionExecute(int opcode); //int opcode is decoded opcode passed from decode()
+	//public void instructionExecute(int opcode); //int opcode is decoded opcode passed from decode()
 	
-	public void instructionWriteBack(Operand result); //Only required for arithemtic instructions; store the result
+	//public void instructionWriteBack(Operand result); //Only required for arithemtic instructions; store the result
 	
 	public InstructionRegister getIR();//To access IR; for testing purposes
 	
