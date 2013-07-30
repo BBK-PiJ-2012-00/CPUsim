86ec7ca49e975235f5f5561e5a3524e57f889507
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-30 22:33:23 +0100

    Starting to implement code for loading memory with Data.

--- code/LoaderImpl.java
+++ code/LoaderImpl.java
@@ -1,6 +1,11 @@
 package code;
 
 public class LoaderImpl implements Loader {
+	private MainMemory memory = MemoryModule.getInstance();
+	
+	public void loadToMemory(Data dataItem) {
+		
+	}
 	
 	
 
