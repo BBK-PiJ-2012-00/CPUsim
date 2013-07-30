f87fd8e777a1b712c01c9b38d4106a715ee0078a
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-30 22:47:35 +0100

    Added display() method, for initial command line interface.

--- code/MemoryModule.java
+++ code/MemoryModule.java
@@ -77,5 +77,14 @@ public class MemoryModule implements MainMemory {
 		return false;
 	}
 	
+	
+	
+	public void display() { //Display memory on command line interface
+		System.out.println("  -- MAIN MEMORY --  ");
+		for (int i = 0; i < MEMORY.length; i++) {
+			System.out.println(i + "| " + MEMORY[i].toString());
+		}
+	}
+	
 
 }
