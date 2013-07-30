c6efad389d292adc8691026784a53b15b53c24ab
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-07-30 22:37:44 +0100

    Updated comments; implement a mechanism that ensures HALT instruction is the final instruction of any program.

--- code/AssemblerImpl.java
+++ code/AssemblerImpl.java
@@ -3,5 +3,8 @@ package code;
 public class AssemblerImpl implements Assembler {
 	private Data[] program; //The assembly language program to be passed to the loader
 	
+	//Check code once translated; ensure that a HALT instruction is the last instruction (if one is missing
+	//in the translated code, insert one by default).
+	
 
 }
