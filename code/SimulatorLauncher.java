91589beb09dda03d502cebc79e1091bf156f8fd2
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-08-03 16:44:41 +0100

    Added test GUI code.

--- code/SimulatorLauncher.java
+++ code/SimulatorLauncher.java
@@ -1,5 +1,10 @@
 package code;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class SimulatorLauncher {
 
 	/**
@@ -15,6 +20,16 @@ public class SimulatorLauncher {
 	private void run() {
 		//Create assembler, loader -> initiate loading of a text file via assembler
 		ControlUnit controlUnit = new ControlUnitImpl(false);
+		MainMemory memory = MemoryModule.getInstance();
+		
+		JLabel testLabel = new JLabel(((MemoryModule) memory).display());
+		
+		JFrame frame = new JFrame("CPUsim");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(testLabel, BorderLayout.WEST);
+		frame.pack();
+		frame.setVisible(true);
+		
 	}
 
 }
