764cb4d51783188c121272e5caa755b0ade83a52
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-08-03 17:00:19 +0100

    Fleshing out code (constructor).

--- code/AssemblerImpl.java
+++ code/AssemblerImpl.java
@@ -1,7 +1,16 @@
 package code;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AssemblerImpl implements Assembler {
-	private Data[] program; //The assembly language program to be passed to the loader
+	private List<Data> programCode; //The assembly language program to be passed to the loader (arraylist expands to fit)
+	
+	
+	
+	public AssemblerImpl() {
+		this.programCode = new ArrayList<Data>();
+	}
 	
 	//Check code once translated; ensure that a HALT instruction is the last instruction (if one is missing
 	//in the translated code, insert one by default).
