b4ae4ac42c46c37ff41b9938cfe0de40c6345815
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-08-03 19:06:55 +0100

    Updated comments.

--- tests/AssemblerTest.java
+++ tests/AssemblerTest.java
@@ -5,15 +5,33 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import code.Assembler;
+import code.AssemblerImpl;
+
 public class AssemblerTest {
+	private Assembler assembler;
+	private String testFile;
 
 	@Before
 	public void setUp() throws Exception {
+		assembler = new AssemblerImpl();
+		testFile = "src/assemblyPrograms/assemblerTestProgram.txt";
 	}
 
+	/*
+	 * Tests file is correctly read, and that each line is added to the arrayList<String> field.
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFileRead() {
+		assembler.selectFile(testFile);
+		assembler.readAssemblyFile();
+		String output = "";
+		for (int i = 0; i < ((AssemblerImpl) assembler).getProgramString().size(); i++) {
+			output += ((AssemblerImpl) assembler).getProgramString().get(i) + "\n";
+			System.out.println(((AssemblerImpl) assembler).getProgramString().get(i));
+		}
+		String expected = "<Label> <Instruction/Variable> <Comments>\nL1:  LOAD r0, [0] #Load r1 with contents of memory address 0\n";
+		assertEquals(expected, output);
 	}
 
 }
