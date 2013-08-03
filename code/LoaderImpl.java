3d4457fa7d77d28a1a22f3a7aa56a20973a2b807
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-08-03 16:58:39 +0100

    Fleshing out code; buffers to hold incoming code from assembler, and methods to load code into main memory.

--- code/LoaderImpl.java
+++ code/LoaderImpl.java
@@ -1,10 +1,21 @@
 package code;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LoaderImpl implements Loader {
 	private MainMemory memory = MemoryModule.getInstance();
+	private List<Data> codeBuffer = new ArrayList<Data>(); //To buffer code coming in from memory (amount not known in advance)
+	//ArrayList grows as required, making it more suitable than an array
+	private Data[] programCode; //An array to represent the program to be loaded into memory
 	
-	public void loadToMemory(Data dataItem) {
-		
+	public void load(Data dataItem) { //For loading code from assmebler to loader
+		codeBuffer.add(dataItem);
+	}
+	
+	public void loadToMemory(Data[] programCode) {
+		this.programCode = programCode;
+		memory.loadMemory(programCode);
 	}
 	
 	
