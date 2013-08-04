7bad7b975649870bdffa3dfbad303e95855f95a5
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-08-04 01:10:33 +0100

    Added comments for partial program logic.

--- code/AssemblerImpl.java
+++ code/AssemblerImpl.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class AssemblerImpl implements Assembler {
@@ -12,11 +14,13 @@ public class AssemblerImpl implements Assembler {
 	private List<String> programString; //Holds program code as string array list, for parsing into data array
 	//Reference to file
 	private String fileReference;
+	private Map<String, Integer> lookupTable; //For associating labels with relative addresses
 	
 	
 	public AssemblerImpl() {
 		this.programCode = new ArrayList<Data>();
 		this.programString = new ArrayList<String>();
+		this.lookupTable = new HashMap<String, Integer>();
 	}
 	
 	
@@ -51,6 +55,32 @@ public class AssemblerImpl implements Assembler {
 	public List<String> getProgramString() {
 		return this.programString;
 	}
+	
+
+/*
+ * Assembler reads file of format below. Each line is read into a String, which is stored in an array list.
+ * Therefore, the index of each string forms its line number, which also serves as a relative address (i.e. the 
+ * code at line 0 is the start address, the code at line 1 is the start address + 1 etc).  If a label is present,
+ * it should be stored in a HashMap as a key (so that when it is looked up, the line number value is returned)
+ * along with the line number of the label as the value. So for example, in the code
+ * below, when line 0 is read from the array list for parsing into an instruction or operand, a label will be detected.
+ * Thus, L1 (string) will be stored in the hash map as a key, with the line number 0 being stored as the value.  When
+ * L1 is later referred to in, say, a branch instruction. the hash map can be consulted so that the label can be
+ * translated to a relative address (0 in this case), and this address can be placed in the instruction field instead
+ * of the label.
+ */
+//	<Label>  <Instruction>
+//	L1:      LOAD r0, var1  #Presence of a label before an instruction indicates it will likely be a branch target
+//	         LOAD r1, var2
+//	         ADD r2, r1
+//	         STORE r1, var3
+//	         BRZ L1 #Branch to L1, which is the LOAD r0, r1 instruction
+//	         HALT
+//
+//	var1:    DATA 9
+//	var2:    DATA 13
+//	var3:    DATA 0 #The target location of a STORE instr. can be declared and initialised to 0
+	         
 	
 	
 	//Check code once translated; ensure that a HALT instruction is the last instruction (if one is missing
