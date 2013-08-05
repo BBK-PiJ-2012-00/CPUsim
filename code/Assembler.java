50f2c629eb86f0e7d12ab2be5e85fab25db60b88
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-08-05 17:09:44 +0100

    Added current methods to interface.

--- code/Assembler.java
+++ code/Assembler.java
@@ -1,5 +1,7 @@
 package code;
 
+import java.util.List;
+
 /*
  * Reads a text file containing assembly language. Can initially read into an array of Strings, from which an array of Instructions is
  * then created.
@@ -10,4 +12,19 @@ public interface Assembler {
 	public void selectFile(String fileName);
 	
 	public void readAssemblyFile();
+	
+	/*
+	 * Method takes a line of code and splits it into an array list of Strings, which can then be passed
+	 * to another method for interpretation (i.e. so that instructions or operands can be created).
+	 */
+	public List<String> splitCodeLine(String line);
+	
+	public List<String> getProgramString();
+	
+	public void assembleCode();
+	
+	public Data createData(List<String> splitData);
+	
+	public void loadToLoader(Data[] programCode);
+
 }
