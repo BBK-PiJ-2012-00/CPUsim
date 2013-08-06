8b9a85a6d219bf643ce982792583301a9758092e
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-08-06 13:32:14 +0100

    Updated all tests to reflect use of symbolic memory addressing; instruction assembly tested successfully.

--- tests/AssemblerTest.java
+++ tests/AssemblerTest.java
@@ -37,8 +37,8 @@ public class AssemblerTest {
 			output += ((AssemblerImpl) assembler).getProgramString().get(i) + "\n";
 			//System.out.println(((AssemblerImpl) assembler).getProgramString().get(i));
 		}
-		String expected = "<Label> <Instruction/Variable> <Comments>\nL1:  LOAD r0, [0] #Load r1 with contents of memory address 0\n" +
-				"var1: DATA 7\n";
+		String expected = "<Label> <Instruction/Variable> <Comments>\nL1:  LOAD r0, var1 #Load r1 with contents " + 
+				"of memory address referred to by var1\n" +	"var1: DATA 7\n";
 		assertEquals(expected, output);
 	}
 	
@@ -49,13 +49,13 @@ public class AssemblerTest {
 	@Test
 	public void testSplitCodeLine() {
 		assembler.readAssemblyFile();
-		String testLine = "L0: LOAD r0, [0] #Comments to be omitted.";
+		String testLine = "L0: LOAD r0, var1 #Comments to be omitted.";
 		List<String> output = assembler.splitCodeLine(testLine);
 		List<String> expected = new ArrayList<String>();
 		expected.add("L0:");
 		expected.add("LOAD");
 		expected.add("r0");
-		expected.add("[0]");
+		expected.add("var1");
 		
 		//assertEquals(expected.size(), output.size());	//This tests correctly but is not as conclusive as the following
 		for (int i = 0; i < output.size(); i++) {
@@ -91,7 +91,7 @@ public class AssemblerTest {
 		
 		List<String> outputArray = assembler.getInstructionArray();
 		String output = outputArray.get(0);
-		String expected = "L1:  LOAD r0, [0] #Load r1 with contents of memory address 0";
+		String expected = "L1:  LOAD r0, var1 #Load r1 with contents of memory address referred to by var1";
 		
 		assertEquals(expected, output);		
 	}
@@ -127,6 +127,21 @@ public class AssemblerTest {
 		assembler.assembleOperand(operandParts);
 		int output = assembler.getLookupTable().get("var1");
 		int expected = 1; //var1 should be mapped to 1 (an instruction would be at address 0 in the program code)
+		
+		assertEquals(expected, output);
+	}
+	
+	
+	@Test
+	public void testAssembleInstruction() {
+		assembler.readAssemblyFile();
+		assembler.separateOperands();
+		List<String> operandParts = assembler.splitCodeLine(assembler.getOperandArray().get(0));
+		List<String> instructionParts = assembler.splitCodeLine(assembler.getInstructionArray().get(0));
+		assembler.assembleOperand(operandParts); //Operand must be created for instruction to be able to be created
+		
+		String expected = new TransferInstr(Opcode.LOAD, 0, 1).toString();
+		String output = assembler.assembleInstruction(instructionParts, 0).toString(); //Create the instruction
 		
 		assertEquals(expected, output);
 	}
