d46a4ca80d1eaf3ec2e698051f823965fb03bb85
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-08-07 14:57:16 +0100

    Load() method tested successfully.

--- tests/LoaderTest.java
+++ tests/LoaderTest.java
@@ -7,18 +7,41 @@ import org.junit.Test;
 
 import code.Data;
 import code.Loader;
+import code.LoaderImpl;
 import code.MainMemory;
 import code.MemoryModule;
+import code.Instruction;
+import code.TransferInstr;
+import code.ArithmeticInstr;
+import code.Opcode;
 
 public class LoaderTest {
+	private Loader loader;
+	private MainMemory memory;
+	private Data[] testProgram;	
 
 	@Before
 	public void setUp() throws Exception {
+		loader = new LoaderImpl();
+		memory = MemoryModule.getInstance();
+		testProgram = new Data[3];
+		testProgram[0] = new TransferInstr(Opcode.LOAD, 0, 0);
+		testProgram[1] = new TransferInstr(Opcode.LOAD, 1, 1);
+		testProgram[2] = new ArithmeticInstr(Opcode.DIV, 1, 0);
+		
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testLoad() { 
+		loader.load(testProgram);
+		
+		Data[] expected = testProgram;
+		Data[] output = loader.getProgramCode();
+		
+		for (int i = 0; i < testProgram.length; i++) {
+			assertEquals(expected[i].toString(), output[i].toString());
+		}
+		
 	}
 
 }
