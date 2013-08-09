ff4ca5253044c197f7b059554991ae7d6e1412cb
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-08-09 01:55:47 +0100

    Trying out GUI code; memory display updating successfully.

--- code/SimulatorLauncher.java
+++ code/SimulatorLauncher.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class SimulatorLauncher {
 
@@ -23,15 +25,42 @@ public class SimulatorLauncher {
 		MainMemory memory = MemoryModule.getInstance();
 		
 		Assembler assembler = new AssemblerImpl();
+		assembler.selectFile("src/assemblyPrograms/assemblerTestProgram2");
+		assembler.assembleCode();
+		assembler.loadToLoader();
+		((AssemblerImpl) assembler).getLoader().loadToMemory();
+		
+		JLabel testLabel = new JLabel(((MemoryModule) memory).display());
+		//JTextArea memoryArea = new JTextArea(10, 50);
+		//memoryArea.add(testLabel);
+		JScrollPane memoryScrollPane = new JScrollPane(testLabel);
+		
+
+//				setPreferredSize(new Dimension(450, 110));
+//				...
+//				add(scrollPane, BorderLayout.CENTER);
+//)
+		
+		JFrame frame = new JFrame("CPUsim");
+		frame.setSize(400, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(memoryScrollPane, BorderLayout.WEST);
+		frame.pack();
+		frame.setVisible(true);
 		
 		
-//		JLabel testLabel = new JLabel(((MemoryModule) memory).display());
-//		
-//		JFrame frame = new JFrame("CPUsim");
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		frame.getContentPane().add(testLabel, BorderLayout.WEST);
-//		frame.pack();
-//		frame.setVisible(true);
+		controlUnit.activate();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		testLabel.setText(((MemoryModule) memory).display());
+		
+		
 		
 	}
 
