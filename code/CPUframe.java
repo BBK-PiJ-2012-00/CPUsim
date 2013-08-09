78588b03743a7a3202bc210605f22f7f150e7fb9
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-08-09 16:18:19 +0100

    Can open an assembly file and execute it on the GUI.

--- code/CPUframe.java
+++ code/CPUframe.java
@@ -2,8 +2,10 @@ package code;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,18 +15,32 @@ import javax.swing.JScrollPane;
 public class CPUframe extends JFrame {
 	private ControlUnit controlUnit;
 	private MainMemory memory;
+	private Assembler assembler;
 	
 	private static final int FRAME_WIDTH = 700;
 	private static final int FRAME_HEIGHT = 400;
 	
+	private JFileChooser fileChooser;
+	
 	private JPanel controlPanel;
 	private JPanel memoryPanel;
+	private JPanel registerPanel;
+	
 	private JLabel memoryLabel;
+	private JLabel MARlabel;
+	private JLabel MBRlabel;
+	private JLabel StatusRegisterlabel;
+	private JLabel PClabel;
+	private JLabel generalPurposeLabel;
+	
 	private JButton executeButton;
 	private JButton resetButton;
+	private JButton fileOpenButton;
+
 	private JScrollPane scroller;
 	
-	public CPUframe(ControlUnit controlUnit, MainMemory memory) {
+	public CPUframe(Assembler assembler, ControlUnit controlUnit, MainMemory memory) {
+		this.assembler = assembler;
 		this.controlUnit = controlUnit;
 		this.memory = memory;
 				
@@ -38,17 +54,34 @@ public class CPUframe extends JFrame {
 		memoryPanel = new JPanel(); 
 		memoryLabel = new JLabel(((MemoryModule) MemoryModule.getInstance()).display());
 		memoryPanel.add(memoryLabel);
+		registerPanel = new JPanel();
 	
 		scroller = new JScrollPane(memoryPanel);		
 		this.getContentPane().add(scroller, BorderLayout.EAST); 
+		
+		MARlabel = new JLabel(MAR.getInstance().display());
+		registerPanel.add(MARlabel);
+
 		
 		executeButton = new JButton("Execute Program");
 		executeButton.addActionListener(new ExecuteListener());
 		controlPanel.add(executeButton);
 		resetButton = new JButton("Reset");
 		resetButton.addActionListener(new ResetListener());
+		
+		fileOpenButton = new JButton("Select Assembly File");
+		fileOpenButton.addActionListener(new FileOpenListener());
+		controlPanel.add(fileOpenButton);
+		
+		
 		controlPanel.add(resetButton);
+		this.getContentPane().add(registerPanel);
 		this.getContentPane().add(controlPanel);
+		
+		fileChooser = new JFileChooser("src/assemblyPrograms");		
+		//int returnVal = fileChooser.showOpenDialog(this); //Opens file chooser on program launch
+				
+		
 		
 	}
 	
@@ -71,5 +104,32 @@ public class CPUframe extends JFrame {
 			memoryLabel.setText(memory.display());
 		}
 	}
+	
+	class FileOpenListener implements ActionListener {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+		    //Handle open button action.
+		    if (e.getSource() == fileOpenButton) {
+		        int returnVal = fileChooser.showOpenDialog(fileOpenButton);
+
+		        if (returnVal == JFileChooser.APPROVE_OPTION) {
+		            File file = fileChooser.getSelectedFile();
+		            
+		            assembler.selectFile(file);
+		    		assembler.assembleCode();
+		    		assembler.loadToLoader();
+		    		((AssemblerImpl) assembler).getLoader().loadToMemory();
+		    		memoryLabel.setText(memory.display()); //update memory display on opening file
+		            
+//		            //This is where a real application would open the file.
+//		            log.append("Opening: " + file.getName() + "." + newline);
+		        } //else {
+//		            log.append("Open command cancelled by user." + newline);
+//		        }
+		   }
+		}
+	}
+	
 
 }
