25b3003bf53f6027cb5536d32cb2831d172f2829
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-08-13 14:48:03 +0100

    Added overloaded constructor to allow general purpose register references to be passed into event, so that the right text field on the gui can be updated.

--- code/ModuleUpdateEvent.java
+++ code/ModuleUpdateEvent.java
@@ -4,14 +4,25 @@ import java.util.EventObject;
 
 public class ModuleUpdateEvent extends EventObject {
 	private String update; //The String with the update value
+	private int register; //For general purpose registers
 
 	public ModuleUpdateEvent(Object source, String update) {
 		super(source);
 		this.update = update;
 	}
 	
+	public ModuleUpdateEvent(Object source, int register, String update) {
+		super(source);
+		this.update = update;
+		this.register = register;
+	}
+	
 	public String getUpdate() {
 		return this.update;
+	}
+	
+	public int getRegisterReference() {
+		return this.register;
 	}
 
 }
