36fd6a9a7cc5ccf76736a5809a8d3d08087ca895
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-08-13 17:00:58 +0100

    Added in wait() after every update to any module.

--- code/ExecuteStage.java
+++ code/ExecuteStage.java
@@ -32,13 +32,21 @@ public abstract class ExecuteStage implements Runnable {
 		this.writeBackStage = writeBackStage;
 	}
 	
-	public boolean instructionExecute(int opcode) {
+	public synchronized boolean instructionExecute(int opcode) {
 		switch (opcode) {
 		
 		
 			case 1: //A LOAD instruction
 					mar.write(ir.read().getField1()); //Load mar with source address of instruction in IR
 					//Request a read from memory via system bus, with address contained in mar
+					
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
 					systemBus.transferToMemory(mar.read(), null);
 					
 					try {
@@ -50,9 +58,25 @@ public abstract class ExecuteStage implements Runnable {
 					
 					mar.write(-1); //Reset MAR
 					
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
 					if (ir.read().getField2() == 16) { //ConditionCodeRegister reference
 						statusRegister.write((Operand) mbr.read()); //Write operand in mbr to condition/status register
+						
+						try {
+							wait();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
 					}
+					
+					
 					
 					//Transfer data in mbr to destination field in instruction in ir (field2).
 					genRegisters.write(ir.read().getField2(), mbr.read());//getField2() gives reg. destination index, mbr.read()
@@ -72,16 +96,58 @@ public abstract class ExecuteStage implements Runnable {
 					
 			case 2: //A STORE instruction
 					mar.write(ir.read().getField2()); //Load mar with destination (memory address)
+					
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
 					mbr.write(genRegisters.read(ir.read().getField1())); //Write to mbr the data held in genRegisters at index
 					//given by field1(source) of instruction held in IR.
+					
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					
 					systemBus.transferToMemory(mar.read(), mbr.read()); //Transfer contents of mbr to address specified in mar
+					
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
 					break;
 					
 			case 3: //A MOVE instruction (moving data between registers)
 					genRegisters.write(ir.read().getField2(), genRegisters.read(ir.read().getField1()));
 					//Write to the destination specified in field2 of instr held in ir, the instr held in the register
 					//specified by field1 of the instruction in the ir.
+					
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					
 					genRegisters.write(ir.read().getField1(), null); //Complete the move by resetting register source
+					
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
 					break;
 					
 					
@@ -91,59 +157,133 @@ public abstract class ExecuteStage implements Runnable {
 					Operand op2 = (Operand) genRegisters.read(ir.read().getField2());//access operand stored in second register
 					Operand result = ALU.AdditionUnit(op1, op2); //Have ALU perform operation
 					writeBackStage.receive(result); //Call write back stage to store result of addition
+					writeBackStage.run();
+					
+					try { //Makes more sense to put the wait here than complicate write back stage
+						wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
 					break;
 			
 			case 5: //A SUB instruction (subtracting contents of one register from a second (storing in the first).
 					op1 = (Operand) genRegisters.read(ir.read().getField1()); //access first operand
 					op2 = (Operand) genRegisters.read(ir.read().getField2()); //access second operand
 					result = ALU.SubtractionUnit(op1, op2);
-					writeBackStage.receive(result);;
+					writeBackStage.receive(result);
+					writeBackStage.run();
+					
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
 					break;				
 					
 			case 6: //A DIV instruction (dividing contents of one register by contents of another (storing in the first).
 					op1 = (Operand) genRegisters.read(ir.read().getField1()); //access first operand
 					op2 = (Operand) genRegisters.read(ir.read().getField2()); //access second operand
 					result = ALU.DivisionUnit(op1, op2); //op1 / op2
-					writeBackStage.receive(result);;
+					writeBackStage.receive(result);
+					writeBackStage.run();
+					
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
 					break;					
 					
 			case 7: //A MUL instruction (multiplying contents of one register by contents of another (storing result in first).
 					op1 = (Operand) genRegisters.read(ir.read().getField1()); //access first operand
 					op2 = (Operand) genRegisters.read(ir.read().getField2()); //access second operand
 					result = ALU.MultiplicationUnit(op1, op2); //op1 * op2
-					writeBackStage.receive(result);;
+					writeBackStage.receive(result);
+					writeBackStage.run();
+					
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
 					break;
 					
 					
 					
 			case 8: //A BR instruction (unconditional branch to memory location in instruction field 1).
 					pc.setPC(ir.read().getField1());
+					
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
 					//A branch instruction updates PC to new memory location
 					break;
 				
 			case 9: //A BRZ instruction (branch if value in status register is zero).
 					if (statusRegister.read().unwrapInteger() == 0) {
 						pc.setPC(ir.read().getField1()); //If statusRegister holds 0, set PC to new address held in instruction
+						
+						try {
+							wait();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
 					}
 					break; //If not 0, do nothing
 					
 			case 10: //A SKZ instruction (skip the next instruction (increment PC by one) if status register holds 0).
 					 if (statusRegister.read().unwrapInteger() == 0) {
 						 pc.incrementPC();
+						 
+						 try {
+								wait();
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						 
 					 }
 					 break; //If not 0, do nothing
 					 
 			case 11: //A BRE instruction (branch if status reg. contents = contents of register ref. in instruction)
 					 int genRegRef = ir.read().getField2(); //Reference to register referred to in instruction
 					 if (statusRegister.read().equals((Operand) genRegisters.read(genRegRef))) { //If equal
-						 pc.setPC(ir.read().getField1()); //Set PC to equal address in field1 of instruction in ir						 
+						 pc.setPC(ir.read().getField1()); //Set PC to equal address in field1 of instruction in ir	
+						 
+						 try {
+								wait();
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
 					 }
 					 break; //If not equal, do nothing
 					 
 			case 12: //A BRNE instruction (branch if status reg. contents != contents of register ref. in instruction)
 					 genRegRef = ir.read().getField2(); //Reference to register referred to in instruction
 					 if (!(statusRegister.read().equals((Operand) genRegisters.read(genRegRef)))) { //If not equal
-						 pc.setPC(ir.read().getField1()); //Set PC to equal address in field1 of instruction in ir						 
+						 pc.setPC(ir.read().getField1()); //Set PC to equal address in field1 of instruction in ir		
+						 
+						 try {
+								wait();
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						 
 					 }
 					 break; //If equal, do nothing
 					 
