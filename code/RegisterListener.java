5f502f1ff571f084f6673c8a3f55a6f651fec8db
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-08-15 14:27:15 +0100

    Added handling for resetting ALU fields.

--- code/RegisterListener.java
+++ code/RegisterListener.java
@@ -3,6 +3,7 @@ package code;
 import java.util.EventListener;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class RegisterListener implements EventListener {
 	private CPUframe frame; //Reference to main window frame object
@@ -42,7 +43,11 @@ public class RegisterListener implements EventListener {
 		 * Object instance is used) so this if statement must come last to avoid use by any other instances.
 		 */
 		else if (e.getSource() instanceof Object) { //ALU (static, therefore no object instantiation; dummy object used)
-			System.out.println("In registerListener from ALU");
+			if (e.getAluUnit().equals("")) { //indicates reset operation
+				for (int i = 0; i < 12; i ++) {
+					frame.getAllAluFields()[i].setText("");				
+				}
+			}
 			if (e.getAluUnit().equals("add")) { //addition unit update
 				frame.getAddFields()[0].setText(e.getOp1().toString());
 				frame.getAddFields()[1].setText(e.getOp2().toString());
