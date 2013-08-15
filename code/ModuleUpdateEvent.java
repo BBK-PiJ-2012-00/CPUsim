821e17c17e696509744582d2d19ee315eb7f5220
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-08-15 14:06:52 +0100

    Updated constructor so that rather than using a null object, an object of type Object is instantiated to avoid null pointer exceptions. Null was used in the first place because all the ALU is never instantiated and relies on static fields and methods.

--- code/ModuleUpdateEvent.java
+++ code/ModuleUpdateEvent.java
@@ -12,9 +12,9 @@ public class ModuleUpdateEvent extends EventObject {
 	private String update; //The String with the update value
 	private int register; //For general purpose registers
 	
-	private int op1; //op1 is the first ALU operand (to be sent for display on GUI)
-	private int op2; //op2 is the second ALU operand
-	private int result; //the result of the ALU operation
+	private Operand op1; //op1 is the first ALU operand (to be sent for display on GUI)
+	private Operand op2; //op2 is the second ALU operand
+	private Operand result; //the result of the ALU operation (these are all type Operand for correct display on GUI)
 	private String aluUnit; //Which unit (i.e. adder) did the event originate from
 
 	public ModuleUpdateEvent(Object source, String update) {
@@ -31,11 +31,11 @@ public class ModuleUpdateEvent extends EventObject {
 	/*
 	 * A constructor for ALU-triggered events.
 	 */
-	public ModuleUpdateEvent(Object source, String aluUnit, int op1, int op2, int result) {
-		super(source);
+	public ModuleUpdateEvent(Object source, String aluUnit, int op1, int op2, Operand result) {
+		super(new Object()); //will be null as ALU is a static utility class (but using null creates problems, hence dummy Obj)
 		this.aluUnit = aluUnit;
-		this.op1 = op1;
-		this.op2 = op2;
+		this.op1 = new OperandImpl(op1);
+		this.op2 = new OperandImpl(op2);
 		this.result = result;
 	}
 	
@@ -45,6 +45,22 @@ public class ModuleUpdateEvent extends EventObject {
 	
 	public int getRegisterReference() {
 		return this.register;
+	}
+	
+	public String getAluUnit() {
+		return this.aluUnit;
+	}
+	
+	public Operand getResult() {
+		return this.result;
+	}
+	
+	public Operand getOp1() {
+		return this.op1;
+	}
+	
+	public Operand getOp2() {
+		return this.op2;
 	}
 
 }
