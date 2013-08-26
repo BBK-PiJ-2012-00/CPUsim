0da18d2850572447a347724aac184cf3c0d5e6e7
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-08-26 13:30:58 +0100

    Updated constructor to take a MainMemory parameter.

--- code/LoaderImpl.java
+++ code/LoaderImpl.java
@@ -8,9 +8,13 @@ import java.util.List;
  * as addressing is initially absolute and not symbolic.
  */
 public class LoaderImpl implements Loader {
-	private MainMemory memory = MemoryModule.getInstance();
+	private MainMemory memory; //Reference to main memory
 	
 	private Data[] programCode; //An array to represent the program to be loaded into memory
+	
+	public LoaderImpl(MainMemory memory) {
+		this.memory = memory;
+	}
 	
 	public void load(Data[] assembledCode) { //For loading code from assmebler to loader
 		this.programCode = assembledCode;
@@ -22,7 +26,7 @@ public class LoaderImpl implements Loader {
 		}
 		//this.programCode = codeBuffer.toArray(new Data[codeBuffer.size()]); //Convert codeBuffer to standard array
 		memory.resetPointer(); //Set memory pointer back to 0
-		memory.loadMemory(programCode);		
+		memory.loadMemory(programCode);	
 	}
 	
 	public Data[] getProgramCode() {
