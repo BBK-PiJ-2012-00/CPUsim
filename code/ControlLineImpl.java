ae4594c74c2cded29a9ab3c8e7d224cf882b3c8f
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-08-29 13:59:04 +0100

    Tidied up comments and line spacing.

--- code/ControlLineImpl.java
+++ code/ControlLineImpl.java
@@ -3,10 +3,8 @@ package code;
 public class ControlLineImpl implements ControlLine {
 	private AddressBus addressBus;
 	private DataBus dataBus;	
-	private MainMemory memory;
-	
+	private MainMemory memory;	
 	private MemoryBufferRegister mbr;//Reference to CPU's MBR
-	//private MemoryAddressRegister mar; //Not required (for the time being)
 	
 	private boolean isWaiting; //So that the SwingWorker thread on the GUI can ascertain if the thread is waiting in this object
 	
@@ -28,9 +26,7 @@ public class ControlLineImpl implements ControlLine {
 	
 	public synchronized boolean writeToBus(int address, Data data) { 
 		if (address == -1) { //Indicates transfer from memory to CPU (2nd phase of memory read; delivery from memory to MBR)
-			//no need to place address on address bus -> if address field in AddressLine is null/0, this signifies delivery
-			//to CPU as opposed to memory (and is true to reality).
-			dataBus.put(data); //This is functionally redundant, but will be useful for GUI animation of bus lines
+			dataBus.put(data); 
 			
 			//fireOperationUpdate("Memory Read");
 			if (data instanceof Instruction) { //Activity monitor commentary
@@ -53,6 +49,7 @@ public class ControlLineImpl implements ControlLine {
 			
 			return this.deliverToMBR(); //Complete read operation. 
 		}
+		
 		else if (data == null) { //Signifies first phase of a read; MAR places address on address line, prompting memory to
 			//place contents of the address on the address line onto data line for return to MBR.
 			addressBus.put(address);
@@ -65,13 +62,13 @@ public class ControlLineImpl implements ControlLine {
 			try {
 				wait();
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			isWaiting = false;
 			
 			return this.deliverToMemory(true);
 		}
+		
 		//Memory write code:
 		addressBus.put(address);
 		dataBus.put(data);
@@ -90,13 +87,13 @@ public class ControlLineImpl implements ControlLine {
 		try {
 			wait();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		isWaiting = false;
 		
 		return this.deliverToMemory(false);	//False -> not a read operation (write operation)	
 	}
+	
 	
 	
 	public boolean deliverToMBR() { //Prompts dataLine to load its value into MBR, completing memory read operation
@@ -113,26 +110,25 @@ public class ControlLineImpl implements ControlLine {
 		return memory.notifyWrite(addressBus.read(), dataBus.read());
 	}
 	
-	
-	public String display() {
-		
-		return null;
-	}
+
 	
 	@Override
 	public AddressBus getAddressBus() {
 		return this.addressBus;
 	}
 	
+	
 	@Override
 	public DataBus getDataBus() {
 		return this.dataBus;
 	}
 	
+	
 	@Override
 	public boolean isWaiting() {
 		return isWaiting;
 	}
+	
 	
 	@Override
 	public void fireActivityUpdate(String update) {
@@ -140,11 +136,13 @@ public class ControlLineImpl implements ControlLine {
 		updateListener.handleUpDateEvent(updateEvent);
 	}
 	
+	
 	@Override
 	public void fireOperationUpdate(String update) {
 		ModuleUpdateEvent updateEvent = new ModuleUpdateEvent(this, true, update); //True reflects update is to control line display
 		updateListener.handleUpDateEvent(updateEvent);
 	}
+	
 	
 	@Override
 	public void registerListener(UpdateListener listener) {
