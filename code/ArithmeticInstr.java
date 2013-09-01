56d0a197abf72be63813f27d1b9aad73fcea3002
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-09-01 16:59:03 +0100

    Toughened restrictions on register references; rCC cannot be referenced by arithmetic instructions.

--- code/ArithmeticInstr.java
+++ code/ArithmeticInstr.java
@@ -16,7 +16,7 @@ public class ArithmeticInstr extends Instruction {
 			throw new IllegalStateException("Invalid opcode for this instruction type.");//Ensures opcode supplied to constructor is an arithmetic opcode
 		}
 		//Restrictions on register reference fields of instruction; must comply with number of general purpose registers in CPU
-		if (registerDest < 0 || registerDest > 16) { //0-15 general purpose, 16 is status register which is user-visible
+		if (registerDest < 0 || registerDest > 15) { //0-15 general purpose
 			throw new IllegalStateException("Invalid register destination reference; must be between 0 and 16 inclusive.");
 		}
 		if (registerInput < 0 || registerInput > 15) {
