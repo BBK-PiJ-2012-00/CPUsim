f2b802255f8e8c81e6d21b9a8608faae2b59968e
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-09-05 14:50:29 +0100

    Tidied.

--- code/AddressBusImpl.java
+++ code/AddressBusImpl.java
@@ -7,20 +7,11 @@ public class AddressBusImpl implements AddressBus {
 	
 	@Override
 	public void put(int address) {
-		//Check address validity?
 		this.address = address; //Fire an update event every time bus is written to
 		ModuleUpdateEvent updateEvent = new ModuleUpdateEvent(this, this.display());
 		updateListener.handleUpDateEvent(updateEvent);
 	}
 	
-	/*
-	 * Pending deletion; not required as default address is now set to -1, signalling that CPU is destination of transfer.
-	 */
-//	@Override
-//	public void put() {
-//		this.address = -1; //-1 not a main memory address; used for transfers to MBR of CPU. Cannot leave as default 0, as 0 is
-//		//a valid memory address.  Set default to -1 instead!
-//	}
 	
 	@Override
 	public int read() {
@@ -29,10 +20,12 @@ public class AddressBusImpl implements AddressBus {
 		return this.address;
 	}
 	
+	
 	@Override
 	public void registerListener(UpdateListener listener) {
 		this.updateListener = listener;
 	}
+	
 	
 	@Override
 	public String display() {
@@ -43,4 +36,6 @@ public class AddressBusImpl implements AddressBus {
 		displayString += this.address;
 		return displayString;
 	}
+	
 }
+
