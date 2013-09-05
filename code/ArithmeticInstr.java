f5c14e35cfa984dddbc0092d6aa371fa46b4cc8f
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-09-05 14:53:17 +0100

    Tidied.

--- code/ArithmeticInstr.java
+++ code/ArithmeticInstr.java
@@ -7,13 +7,14 @@ package code;
  */
 
 public class ArithmeticInstr extends Instruction {
-	private int registerDest; //Register where result of operation should be stored.
-	private int registerInput; //Register where the input operand is stored (the operand to be added/subtracted etc to the contents of registerDest).
+	private int registerDest; //Register where result of operation should be stored
+	private int registerInput; //Register where the input operand is stored
 	
 	public ArithmeticInstr(Opcode opcode, int registerDest, int registerInput) {
 		super(opcode);
 		if (this.OPCODE.getValue() < 4 || this.OPCODE.getValue() > 7) {
-			throw new IllegalStateException("Invalid opcode for this instruction type.");//Ensures opcode supplied to constructor is an arithmetic opcode
+			//Ensures opcode supplied to constructor is an arithmetic opcode
+			throw new IllegalStateException("Invalid opcode for this instruction type.");
 		}
 		//Restrictions on register reference fields of instruction; must comply with number of general purpose registers in CPU
 		if (registerDest < 0 || registerDest > 15) { //0-15 general purpose
