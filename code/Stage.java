e4795a0d93170aab742f069ea90cae3eac9b7b07
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-09-08 00:21:03 +0100

    Added getters and setters for common fields.

--- code/Stage.java
+++ code/Stage.java
@@ -15,6 +15,10 @@ public abstract class Stage implements Runnable {
 	private static Object lock = new Object(); //Static object that is used to synchronize accessMemory() method
 												//across fetch/decode and execute stage objects.
 	
+	private boolean active;
+	private boolean pipelineFlush;
+	private boolean isWaiting;
+	
 
 	public Stage(BusController systemBus, InstructionRegister ir, ProgramCounter pc, RegisterFile genRegisters,
 			Register statusRegister, MemoryBufferRegister mbr, MemoryAddressRegister mar) {
@@ -44,35 +48,46 @@ public abstract class Stage implements Runnable {
 		synchronized(lock) {
 		
 			if (isInstructionFetch) {
-				mar.write(pc.getValue()); //Write address value in PC to MAR.
+				System.out.println("Entering instructionFetch()");
+				this.fireUpdate("\n** INSTRUCTION FETCH/DECODE STAGE ** \n");
+				//getIR().clear(); //Clear previous instruction from display
+				
+				if (Thread.currentThread().isInterrupted()) { //In event of pipeline flush from execute stage
+					System.out.println("Entering interrupted block 1");
+					fireUpdate("**Branch taken in execute stage; pipeline flush. Current instruction \nfetch/decode abandoned.");
+					pipelineFlush = true;
+					return;
+				}
+				
+				getMAR().write(getPC().getValue()); //Write address value in PC to MAR.
 				
 				if (Thread.currentThread().isInterrupted()) { //In event of pipeline flush from execute stage
 					fireUpdate("**Branch taken in execute stage; pipeline flush. Current instruction \nfetch/decode abandoned.");
-					setPipelineFlush(true);
+					pipelineFlush = true;
 					return;
 				}
 				
 				fireUpdate("Memory address from PC placed into MAR \n");
 				
-	//			setWaitStatus(true);
-	//			try {
-	//				wait();
-	//			} catch (InterruptedException e) {
-	//				e.printStackTrace();
-	//				setWaitStatus(false);
-	//				active = false;
-	//				return; //Do not continue execution if interrupted (SwingWorker.cancel(true) is called).
-	//			}
-	//			setWaitStatus(false);
+//				setWaitStatus(true);
+//				try {
+//					wait();
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//					setWaitStatus(false);
+//					active = false;
+//					return; //Do not continue execution if interrupted (SwingWorker.cancel(true) is called).
+//				}
+//				setWaitStatus(false);
 				
 				if (Thread.currentThread().isInterrupted()) { //In event of pipeline flush from execute stage
 					fireUpdate("Branch taken in execute stage; pipeline flush. Current instruction \nfetch/decode abandoned.");
-					setPipelineFlush(true);
+					pipelineFlush = true;
 					return;
 				}
 				
 				//Transfer address from MAR to system bus, prompting read
-				boolean successfulTransfer = systemBus.transferToMemory(mar.read(), null); 
+				boolean successfulTransfer = getSystemBus().transferToMemory(getMAR().read(), null); 
 				if (!successfulTransfer) { 
 					//If SwingWorker is cancelled and thread of execution is interrupted, successfulTransfer will be false and the
 					//method should not execute any further
@@ -83,37 +98,58 @@ public abstract class Stage implements Runnable {
 				
 				if (Thread.currentThread().isInterrupted()) { //In event of pipeline flush from execute stage
 					fireUpdate("Branch taken in execute stage; pipeline flush. Current instruction \nfetch/decode abandoned.");
-					setPipelineFlush(true);
+					pipelineFlush = true;
 					return;
 				}
 				
-				this.fireUpdate("Load contents of memory address " + mar.read() + " into MBR \n");
+				this.fireUpdate("Load contents of memory address " + getMAR().read() + " into MBR \n");
 				
 				
 				
-	//			setWaitStatus(true);
-	//			try {
-	//				wait();
-	//			} catch (InterruptedException e) {
-	//				e.printStackTrace();
-	//				setWaitStatus(false);
-	//				return; //Do not continue execution if interrupted (SwingWorker.cancel(true) is called).
-	//			}
-	//			setWaitStatus(false);
+//				setWaitStatus(true);
+//				try {
+//					wait();
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//					setWaitStatus(false);
+//					return; //Do not continue execution if interrupted (SwingWorker.cancel(true) is called).
+//				}
+//				setWaitStatus(false);
 				
 				if (Thread.currentThread().isInterrupted()) { //In event of pipeline flush from execute stage
 					fireUpdate("**Branch taken in execute stage; pipeline flush. Current instruction \nfetch/decode abandoned.");
-					setPipelineFlush(true);
+					pipelineFlush = true;
 					return;
 				}
 				
 				
 				
-				System.out.println("Ln102, attempting to cast: " + mbr.read());
+				System.out.println("Ln102, attempting to cast: " + getMBR().read());
 				//A Data item should now be in MBR
-				ir.loadIR((Instruction) mbr.read()); //Cast required as mbr holds type data, IR type Instruction; May need to handle exception
-				System.out.println("Instruction: " + mbr.read());
+				getIR().loadIR((Instruction) getMBR().read()); //Cast required as mbr holds type data, IR type Instruction; May need to handle exception
+				System.out.println("Instruction: " + getMBR().read());
 				this.fireUpdate("Load contents of MBR into IR \n");
+				
+				if (Thread.currentThread().isInterrupted()) { //In event of pipeline flush from execute stage
+					fireUpdate("**Branch taken in execute stage; pipeline flush. Current instruction \nfetch/decode abandoned.");
+					pipelineFlush = true;
+					return;
+				}
+				
+//				setWaitStatus(true);
+//				try {
+//					wait();
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//					setWaitStatus(false);
+//					return; //Do not continue execution if interrupted (SwingWorker.cancel(true) is called).
+//				}
+//				setWaitStatus(false);
+				
+				getMAR().write(-1);//Reset MAR. Repositioned here for user clarity; mem. addr. remains in MAR until instr. in IR.		
+				getMBR().write(null); //Clear MBR to reflect that instruction has moved to IR (should it be reset earlier, to better reflect
+				//movement?)
+				
 			}
 			
 			/*
@@ -251,7 +287,43 @@ public abstract class Stage implements Runnable {
 	
 	protected abstract void fireUpdate(String update);
 	
-	public abstract void setPipelineFlush(boolean isFlush);
+	//public abstract void setPipelineFlush(boolean isFlush);
+	
+	public boolean isActive() {
+		return this.active;
+	}
+	
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	public boolean isPipellineFlush() {
+		return this.pipelineFlush;
+	}
+	
+	public void setPipelineFlush(boolean flush) {
+		this.pipelineFlush = flush;
+	}
+	
+	/*
+	 * This boolean flag allows the GUI's SwingWorker thread to determine whether the thread is
+	 * waiting on this object. If so, notify() will be called on this object (and not otherwise).
+	 */
+	public boolean isWaiting() {
+		return this.isWaiting;
+	}
+	
+	/*
+	 * This method is used when assembly program execution is reset/restarted midway through;
+	 * if isWaiting has just been set to false and the worker thread is cancelled, when it comes to
+	 * stepping through execution after the restart, the "Step" button won't work as it uses the status
+	 * of isWaiting to determine whether to resume execution or not.
+	 */
+	public void setWaiting(boolean waiting) {
+		this.isWaiting = waiting;
+	}
+	
+	
 	
 	public InstructionRegister getIR() {
 		return this.ir;
