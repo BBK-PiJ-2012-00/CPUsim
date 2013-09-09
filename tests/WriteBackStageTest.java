cedaef73313dfd5ccbb742d604983ecc861953c6
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-09-09 11:08:07 +0100

    Refactored and retested standard stage code, and added first pipeline test.

--- tests/WriteBackStageTest.java
+++ tests/WriteBackStageTest.java
@@ -2,26 +2,48 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import code.ArithmeticInstr;
+import code.CPUbuilder;
+import code.ControlUnit;
+import code.ExecuteStage;
+import code.FetchDecodeStage;
 import code.IR;
+import code.IRfile;
 import code.Instruction;
 import code.InstructionRegister;
+import code.MemoryAddressRegister;
+import code.MemoryBufferRegister;
 import code.Opcode;
 import code.Operand;
 import code.OperandImpl;
+import code.PC;
+import code.PipelinedExecuteStage;
+import code.PipelinedFetchDecodeStage;
+import code.PipelinedWriteBackStage;
+import code.ProgramCounter;
+import code.Register;
 import code.RegisterFile;
 import code.RegisterFile16;
+import code.StandardExecuteStage;
+import code.StandardFetchDecodeStage;
 import code.StandardWriteBackStage;
+import code.StatusRegister;
 import code.UpdateListener;
 import code.WriteBackStage;
 
 public class WriteBackStageTest {
 	private WriteBackStage writeBackStage;
+	private WriteBackStage pipelinedWriteBackStage;
+	private BlockingQueue<Instruction> testExecuteToWriteQueue;
 	
 	private InstructionRegister ir;
+	private InstructionRegister irFile;
 	private RegisterFile genRegisters;
 	
 	private Instruction testInstrADD;
@@ -32,12 +54,57 @@ public class WriteBackStageTest {
 
 	@Before
 	public void setUp() throws Exception {
+		/*
+		 * Listeners are added to all classes that use a listener to prevent null exceptions 
+		 * during testing (serve no functional purpose here).
+		 */
+		
+		CPUbuilder builder = new CPUbuilder(false);
+		ControlUnit controlUnit = builder.getControlUnit(); 
+		//MAR/MBR need to be referenced from control unit to ensure no duplicates, which would fail the tests; this is because
+		//the MAR/MBR are also referenced by the system bus and the exact same objects must be used throughout.
+		MemoryAddressRegister mar = controlUnit.getMAR();
+		MemoryBufferRegister mbr = controlUnit.getMBR();
+		mbr.registerListener(new UpdateListener(new TestFrame()));
+		mar.registerListener(new UpdateListener(new TestFrame()));
+		builder.getBusController().accessControlLine().registerListener(new UpdateListener(new TestFrame()));
+		builder.getBusController().accessControlLine().getAddressBus().registerListener(new UpdateListener(new TestFrame()));
+		builder.getBusController().accessControlLine().getDataBus().registerListener(new UpdateListener(new TestFrame()));	
+		
 		ir = new IR();
-		ir.registerListener(new UpdateListener(new TestFrame())); //Dummy listeners instantiated to prevent null pointer exception
+		ir.registerListener(new UpdateListener(new TestFrame()));
+		
 		genRegisters = new RegisterFile16();
 		genRegisters.registerListener(new UpdateListener(new TestFrame()));
 		
-		writeBackStage = new StandardWriteBackStage(ir, genRegisters);	
+		ProgramCounter pc = new PC();
+		pc.registerListener(new UpdateListener(new TestFrame()));
+		
+		Register statusRegister = new StatusRegister();
+		statusRegister.registerListener(new UpdateListener(new TestFrame()));	
+		
+		builder.getMemoryModule().registerListener(new UpdateListener(new TestFrame()));
+		
+		
+		irFile = new IRfile();
+		irFile.registerListener(new UpdateListener(new TestFrame()));
+		
+		BlockingQueue<Instruction> testFetchToExecuteQueue = new SynchronousQueue<Instruction>();
+		testExecuteToWriteQueue = new SynchronousQueue<Instruction>(); //Prevents null pointer exception
+		
+		FetchDecodeStage pipelinedFetchDecodeStage = new PipelinedFetchDecodeStage(builder.getBusController(), irFile, pc, 
+				genRegisters, statusRegister, mbr, mar, testFetchToExecuteQueue);
+		pipelinedFetchDecodeStage.registerListener(new UpdateListener(new TestFrame()));
+		
+		ExecuteStage pipelinedExecuteStage = new PipelinedExecuteStage(builder.getBusController(), irFile, pc, genRegisters, 
+				statusRegister,	mbr, mar, testFetchToExecuteQueue, testExecuteToWriteQueue, pipelinedFetchDecodeStage, writeBackStage);
+		pipelinedExecuteStage.registerListener(new UpdateListener(new TestFrame()));
+		
+		pipelinedWriteBackStage = new PipelinedWriteBackStage(builder.getBusController(), irFile, pc, genRegisters, statusRegister,
+				mbr, mar, testExecuteToWriteQueue);
+		
+		writeBackStage = new StandardWriteBackStage(builder.getBusController(), ir, pc, genRegisters, statusRegister,
+				mbr, mar);	
 		
 		testInstrADD = new ArithmeticInstr(Opcode.ADD, 2, 4); //Add contents of r2 and r4, storing in r2
 		testInstrSUB = new ArithmeticInstr(Opcode.SUB, 9, 10); //Sub contents of r10 from r9, storing in r9
@@ -71,7 +138,33 @@ public class WriteBackStageTest {
 		assertEquals(expected, output);
 	}
 	
-	//Need to test pipelined version once implemented
+	
+	/*
+	 * PIPELINED MODE TESTS
+	 */
+	
+	@Test
+	public void testPipelinedWriteBack() {
+		Thread t = new Thread(pipelinedWriteBackStage);
+		t.start();
+		pipelinedWriteBackStage.receive(new OperandImpl(22)); //Supply stage with a result value
+		try {
+			testExecuteToWriteQueue.put(testInstrDIV); //Put test instruction into queue for passing to wbStage
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			Thread.sleep(500); //Let t run for half a second
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		t.interrupt(); //Signal the thread to stop (handle an interrupt)
+		while(t.isAlive()); //Wait for thread to die; ensures thread actually dies or assert is never reached
+		assertEquals(22, ((Operand) genRegisters.read(3)).unwrapInteger()); //If successful, #22 should be stored in r3.
+		
+	}
 
 	
 	
