566a0e9b4eb252107addad95148749ad21db8250
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-09-09 02:18:15 +0100

    Updated now that Stage is extended.

--- code/StandardWriteBackStage.java
+++ code/StandardWriteBackStage.java
@@ -2,15 +2,34 @@ package code;
 
 public class StandardWriteBackStage extends WriteBackStage {
 
-	public StandardWriteBackStage(InstructionRegister ir, RegisterFile genRegisters) {
-		super(ir, genRegisters);
-		// TODO Auto-generated constructor stub
+	public StandardWriteBackStage(BusController systemBus, InstructionRegister ir, ProgramCounter pc, RegisterFile genRegisters,
+			Register statusRegister, MemoryBufferRegister mbr, MemoryAddressRegister mar) {
+		
+		super(systemBus, ir, pc, genRegisters, statusRegister, mbr, mar);
 	}
 
 	@Override
 	public void receive(Operand result) {
-		//this.instructionWriteBack(result);
-		this.setResult(result);
+		setResult(result); //Set result field in WriteBackStage super class
+	}
+	
+	@Override
+	//Writing of results to register file
+	public void instructionWriteBack(Operand result) { //Not required in every cycle
+		//It is implicit in the nature of arithmetic instructions that the result is stored in the register
+		//referenced in the first field of the instruction after the opcode (field1)
+		getGenRegisters().write(getIR().read().getField1(), result);
+	}
+	
+	@Override
+	public synchronized void run() {
+		instructionWriteBack(getResult());
+	}
+
+	@Override
+	protected void fireUpdate(String update) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 
