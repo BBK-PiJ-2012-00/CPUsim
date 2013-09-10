7d3745247b85e5cf5886cd079f7fe1f3b2e47783
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-09-10 16:39:24 +0100

    Added method fireUpdate() so that updates are done using SwingUtilities.invokeLater(), ensuring that GUI updates are done from the Event Dispatch Thread and not SwingWorker (or any other thread).

--- code/ALU.java
+++ code/ALU.java
@@ -1,5 +1,7 @@
 package code;
 
+import javax.swing.SwingUtilities;
+
 /*
  * Class to represent arithmetic & logic unit. Static methods representing
  * addition unit, multiplication unit etc.
@@ -17,8 +19,9 @@ public class ALU {
 		int sum = op1 + op2;
 		Operand result = new OperandImpl(sum);
 		
-		ModuleUpdateEvent updateEvent = new ModuleUpdateEvent(null, "add", op1, op2, result);
-		updateListener.handleUpDateEvent(updateEvent);		
+//		ModuleUpdateEvent updateEvent = new ModuleUpdateEvent(null, "add", op1, op2, result);
+//		updateListener.handleUpDateEvent(updateEvent);	
+		fireUpdate("add", op1, op2, result);
 		return result;
 	}
 	
@@ -28,8 +31,9 @@ public class ALU {
 		int intResult = op1 - op2;
 		Operand result = new OperandImpl(intResult);
 		
-		ModuleUpdateEvent updateEvent = new ModuleUpdateEvent(null, "sub", op1, op2, result);
-		updateListener.handleUpDateEvent(updateEvent);
+//		ModuleUpdateEvent updateEvent = new ModuleUpdateEvent(null, "sub", op1, op2, result);
+//		updateListener.handleUpDateEvent(updateEvent);
+		fireUpdate("sub", op1, op2, result);
 		return result;
 	}
 	
@@ -39,8 +43,9 @@ public class ALU {
 		int intResult = op1 / op2;
 		Operand result = new OperandImpl(intResult);
 		
-		ModuleUpdateEvent updateEvent = new ModuleUpdateEvent(null, "div", op1, op2, result);
-		updateListener.handleUpDateEvent(updateEvent);
+//		ModuleUpdateEvent updateEvent = new ModuleUpdateEvent(null, "div", op1, op2, result);
+//		updateListener.handleUpDateEvent(updateEvent);
+		fireUpdate("div", op1, op2, result);
 		return result;
 	}
 	
@@ -50,8 +55,9 @@ public class ALU {
 		int intResult = op1 * op2;		
 		Operand result = new OperandImpl(intResult);
 		
-		ModuleUpdateEvent updateEvent = new ModuleUpdateEvent(null, "mul", op1, op2, result);
-		updateListener.handleUpDateEvent(updateEvent);
+//		ModuleUpdateEvent updateEvent = new ModuleUpdateEvent(null, "mul", op1, op2, result);
+//		updateListener.handleUpDateEvent(updateEvent);
+		fireUpdate("mul", op1, op2, result);
 		return result;
 	}
 	
@@ -60,8 +66,20 @@ public class ALU {
 	}
 	
 	public static void clearFields() { //For resetting GUI fields after alu operation
-		ModuleUpdateEvent updateEvent = new ModuleUpdateEvent(null, "", 0, 0, null);
-		updateListener.handleUpDateEvent(updateEvent);
+//		ModuleUpdateEvent updateEvent = new ModuleUpdateEvent(null, "", 0, 0, null);
+//		updateListener.handleUpDateEvent(updateEvent);
+		fireUpdate("", 0, 0, null);
+	}
+	
+	//GUI events should be handled from EDT
+	//This adds the update event to the EDT thread. Need to test this works on the GUI
+	public static void fireUpdate(final String update, final int op1, final int op2, final Operand result) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+			    ModuleUpdateEvent updateEvent = new ModuleUpdateEvent(null, update, op1, op2, result);
+				updateListener.handleUpDateEvent(updateEvent);	
+			}
+		});
 	}
 	
 
