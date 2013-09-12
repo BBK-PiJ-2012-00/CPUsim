1d8ff83ccfb8f0ede1f11763acb04027af75a546
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-09-12 13:24:25 +0100

    Tidied. Complete.

--- code/SimulatorLauncher.java
+++ code/SimulatorLauncher.java
@@ -1,18 +1,12 @@
 package code;
 
-import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 
 public class SimulatorLauncher {
+	//This class launches the program
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 		
 		SimulatorLauncher simLauncher = new SimulatorLauncher();
@@ -27,11 +21,7 @@ public class SimulatorLauncher {
 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack(); //Adjusts size automatically
-		frame.setVisible(true);
-		
-		
-
-		
+		frame.setVisible(true);		
 		
 	}
 
