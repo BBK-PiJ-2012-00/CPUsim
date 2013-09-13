48e20487cea69c359002218bd0b9eb59061338c5
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-09-13 23:48:57 +0100

    Tidying code.

--- code/FetchDecodeStage.java
+++ code/FetchDecodeStage.java
@@ -21,109 +21,19 @@ public abstract class FetchDecodeStage extends Stage {
 			Register rCC, MemoryBufferRegister mbr, MemoryAddressRegister mar) {
 		
 		super(systemBus, ir, pc, genRegisters, rCC, mbr, mar);
-		
-//		this.systemBus = systemBus;
-//		
-//		
-//		
-//		this.mar = mar;
-//		this.mbr = mbr;
-//		
-//		this.ir = ir;
-//		this.pc = pc;
+
 	}
 	
 	
-//	public void instructionFetch() {
-//		this.fireUpdate("\n** INSTRUCTION FETCH/DECODE STAGE ** \n");
-//		getIR().clear(); //Clear previous instruction from display
-//		getMAR().write(getPC().getValue()); //Write address value in PC to MAR.
-//		
-//		this.fireUpdate("Memory address from PC placed into MAR \n");
-//		
-////		isWaiting = true;
-////		try {
-////			wait();
-////		} catch (InterruptedException e) {
-////			e.printStackTrace();
-////			isWaiting = false;
-////			return; //Do not continue execution if interrupted (SwingWorker.cancel(true) is called).
-////		}
-////		isWaiting = false;
-//		
-//		
-//		//Transfer address from MAR to system bus, prompting read
-//		boolean successfulTransfer = getSystemBus().transferToMemory(getMAR().read(), null); 
-//		if (!successfulTransfer) { 
-//			//If SwingWorker is cancelled and thread of execution is interrupted, successfulTransfer will be false and the
-//			//method should not execute any further
-//			return;
-//		}
-//		
-//		System.out.println("Reentered f/d stage from bus: successful transfer = " + successfulTransfer);
-//		
-//		this.fireUpdate("Load contents of memory address " + getMAR().read() + " into MBR \n");
-//		
-//		
-////		isWaiting = true;
-////		try {
-////			wait();
-////		} catch (InterruptedException e) {
-////			e.printStackTrace();
-////			isWaiting = false;
-////			return; //Do not continue execution if interrupted (SwingWorker.cancel(true) is called).
-////		}
-////		isWaiting = false;	
-//		
-//		
-//		//A Data item should now be in MBR
-//		getIR().loadIR((Instruction) getMBR().read()); //Cast required as mbr holds type data, IR type Instruction; May need to handle exception
-//		this.fireUpdate("Load contents of MBR into IR \n");
-//		
-////		isWaiting = true;
-////		try {
-////			wait();
-////		} catch (InterruptedException e) {
-////			e.printStackTrace();
-////			isWaiting = false;
-////			return; //Do not continue execution if interrupted (SwingWorker.cancel(true) is called).
-////		}
-////		isWaiting = false;
-//		
-//		getMAR().write(-1);//Reset MAR. Repositioned here for user clarity; mem. addr. remains in MAR until instr. in IR.		
-//		getMBR().write(null); //Clear MBR to reflect that instruction has moved to IR
-//		
-//	}
-//	//Fetch ends with instruction being loaded into IR.
-//	
-//	
-//	public int instructionDecode() { //Returns int value of opcode
-//		Instruction instr = getIR().read();
-//		int opcodeValue = instr.getOpcode().getValue(); //Gets instruction opcode as int value
-//		getPC().incrementPC(); //Increment PC; done here so that with pipelining, the next instruction can be fetched at this point
-//		this.fireUpdate("PC incremented by 1 (ready for next instruction fetch) \n");
-////		
-////		isWaiting = true;
-////		try {
-////			wait();
-////		} catch (InterruptedException e) {
-////			e.printStackTrace();
-////			isWaiting = false;
-////			return -1; //Do not continue execution if interrupted (SwingWorker.cancel(true) is called). -1 signals to control
-////			//unit to stop execution.
-////		}
-////		isWaiting = false;		
-//		
-//		return opcodeValue;
-//		
-//	}
-	
-//	@Override
-//	public synchronized void run() { //Synchronized to enable step execution
-//		this.instructionFetch();
-//		opcodeValue = this.instructionDecode();
-//	}
-	
+	/*
+	 * Standard and pipelined stages will implement this slightly differently, hence it
+	 * is abstract (both version must have this method, though).
+	 * 
+	 * @return boolean if the stage is interrupted (or flushed in pipelined mode), the
+	 * course of action is to stop executing and return false (this is implemented in an
+	 * error handling block for InterruptedExceptions), signalling to the run() method code
+	 * that it should terminate execution.
+	 */
 	public abstract boolean instructionFetch();
 	
 	public abstract int instructionDecode();
