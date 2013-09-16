0f8cafecef3248ad146ead359db7d283088439f5
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-09-16 07:29:26 +0100

    Complete.

--- code/PC.java
+++ code/PC.java
@@ -6,40 +6,36 @@ public class PC implements ProgramCounter {
 	private int nextInstructionPointer; //address of the next instruction to be fetched
 	private UpdateListener updateListener;
 	
+	
 	public int getValue() {
 		return nextInstructionPointer;
 	}
 	
+	
+	
 	public void incrementPC() {
 		nextInstructionPointer++;
-//		System.out.println("PC incremented to address: " + nextInstructionPointer);
-//		ModuleUpdateEvent updateEvent = new ModuleUpdateEvent(this, "" + nextInstructionPointer);
-//		updateListener.handleUpDateEvent(updateEvent);
 		fireUpdate("" + nextInstructionPointer);
 	}
+	
+	
 	
 	public void setPC(int address) { //Used to set PC pointer
 		this.nextInstructionPointer = address;
-		//System.out.println("PC set to address " + address);
-//		ModuleUpdateEvent updateEvent = new ModuleUpdateEvent(this, "" + nextInstructionPointer);
-//		updateListener.handleUpDateEvent(updateEvent);
 		fireUpdate("" + nextInstructionPointer);
 	}
 	
-	public String display() {
-		String display = "";
-		display += nextInstructionPointer;
-		return display;
-	}
+	
 	
 	public void registerListener(UpdateListener listener) {
 		this.updateListener = listener;
 	}
 	
-	//GUI events should be handled from EDT
-	//This adds the update event to the EDT thread. Need to test this works on the GUI
-	@Override
-	public void fireUpdate(final String update) {
+	
+	
+	//GUI events should be handled from EDT only.
+	//This adds the update event to the EDT thread.
+	private void fireUpdate(final String update) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 			    ModuleUpdateEvent updateEvent = new ModuleUpdateEvent(PC.this, update);
--- code/ProgramCounter.java
+++ code/ProgramCounter.java
@@ -11,11 +11,13 @@ public interface ProgramCounter {
 	 */
 	public int getValue();
 	
+	
 	/*
 	 * Increments the address held in the PC by one, to fetch
 	 * the next sequential instruction from memory.
 	 */
 	public void incrementPC();
+	
 	
 	/*
 	 * A method used to set the value of the PC; used when a program is 
@@ -23,10 +25,16 @@ public interface ProgramCounter {
 	 */
 	public void setPC(int address);
 	
+	
+	/*
+	 * A method for registering an event listener object with the PC,
+	 * for GUI display purposes. Every time the contents of the PC is updated,
+	 * an update event is created and handled by the listener to change the GUI
+	 * display accordingly.
+	 * 
+	 * @param UpdateListener listener the listener object to handle update events.
+	 */
 	public void registerListener(UpdateListener listener);
 	
-	public String display();
 	
-	public void fireUpdate(String update);
-
 }
