7d0778885c3d3cc84ea322196e4720470a29d7a6
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-09-16 08:37:04 +0100

    Complete.

--- code/FetchDecodeStage.java
+++ code/FetchDecodeStage.java
@@ -1,20 +1,12 @@
 package code;
 
-public abstract class FetchDecodeStage extends Stage {
-//	private BusController systemBus;
-//	
-//	private MemoryAddressRegister mar;
-//	private MemoryBufferRegister mbr;	
-//	
-//	private InstructionRegister ir;
-//	private ProgramCounter pc;
-	
-	
-	private int opcodeValue; //This is accessed by control unit to pass to next stage.
-	
-	private UpdateListener updateListener; //Update event listener
-	
-	//private boolean isWaiting;
+/*
+ * A stage object representing the fetch/decode stage of the instruction cycle. Is subclassed
+ * by the pipelined and standard variations.
+ */
+public abstract class FetchDecodeStage extends Stage {	
+	private int opcodeValue; //This is accessed by control unit to pass to next stage in standard mode.	
+	private UpdateListener updateListener; 	
 	
 	
 	public FetchDecodeStage(BusController systemBus, InstructionRegister ir, ProgramCounter pc, RegisterFile genRegisters,
@@ -36,70 +28,53 @@ public abstract class FetchDecodeStage extends Stage {
 	 */
 	public abstract boolean instructionFetch();
 	
+	
+	/*
+	 * Contains the code for the decoding of instructions fetched from memory and that are being
+	 * held in the IR.
+	 * 
+	 * @return int has a value of -1 in the event of a reset (causing an interrupt) or pipeline
+	 * flush. Otherwise is the true value of the decoded instruction's opcode.
+	 */
 	public abstract int instructionDecode();
 	
+	
+	/*
+	 * @return int the opcodeValue field, representing the numerical value of an instruction's
+	 * opcode.
+	 */
 	public int getOpcodeValue() {
 		return this.opcodeValue;
 	}
 	
+	
+	/*
+	 * @param int opcodeValue the value to be taken on by the opcodeValue field.
+	 */
 	public void setOpcodeValue(int opcodeValue) {
 		this.opcodeValue = opcodeValue;
 	}
 	
-	@Override
-	protected abstract void fireUpdate(String update); 
+
 	
+	/*
+	 * A method for registering an event listener object with the stage object,
+	 * for GUI display purposes. Every time an activity monitor comment is generated,
+	 * an update event is created and handled by the listener to change the GUI
+	 * display accordingly.
+	 * 
+	 * @param UpdateListener listener the listener object to handle update events.
+	 */
 	public void registerListener(UpdateListener listener) {
 		this.updateListener = listener;
 	}
 	
+	
+	/*
+	 * @return UpdateListener the updateListener field.
+	 */
 	public UpdateListener getUpdateListener() {
 		return this.updateListener;
 	}
-	
-	
-	
-	/*
-	 * This boolean flag allows the GUI's SwingWorker thread to determine whether the thread is
-	 * waiting on this object. If so, notify() will be called on this object (and not otherwise).
-	 */
-//	public boolean isWaiting() {
-//		return isWaiting;
-//	}
-	
-	/*
-	 * This method is used when assembly program execution is reset/restarted midway through;
-	 * if isWaiting has just been set to false and the worker thread is cancelled, when it comes to
-	 * stepping through execution after the restart, the "Step" button won't work as it uses the status
-	 * of isWaiting to determine whether to resume execution or not.
-	 */
-//	public void setWaitStatus(boolean status) {
-//		isWaiting = status;
-//	}
-	
-	
-//	public BusController getSystemBus() {
-//		return this.systemBus;
-//	}
-//	
-//	public MemoryAddressRegister getMAR() {
-//		return this.getMAR();
-//	}
-//	
-//	public MemoryBufferRegister getMBR() {
-//		return this.mbr;
-//	}
-//	
-//	public InstructionRegister getIR() {
-//		return this.ir;
-//	}
-//	
-//	public ProgramCounter getPC() {
-//		return this.pc;
-//	}
-	
-//	public UpdateListener getUpdateListener() {
-//		return this.updateListener;
-//	}
 	
 }
