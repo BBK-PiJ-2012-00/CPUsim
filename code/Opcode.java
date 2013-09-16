e56ba6338c02322caf2304ce1830c7f1d666c26b
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-09-16 07:08:17 +0100

    Complete.

--- code/Opcode.java
+++ code/Opcode.java
@@ -1,5 +1,8 @@
 package code;
 
+/*
+ * An enum representing instruction opcode values.
+ */
 public enum Opcode {
 	
 	LOAD(1), STORE(2), MOVE(3),
@@ -7,13 +10,13 @@ public enum Opcode {
 	BR(8), BRZ(9), SKZ(10), BRE(11), BRNE(12), 
 	HALT(13);
 	
-	private int value;
+	private int value; //Associates an integer value with each opcode
 	
-	private Opcode(int value) {
+	private Opcode(int value) { //A constructor allowing the value field to be set
 		this.value = value;
 	}
 	
-	public int getValue() {
+	public int getValue() {  //Returns the integer value associated with each opcode
 		return this.value;
 	}
 
