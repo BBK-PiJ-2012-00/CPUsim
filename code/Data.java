afc543c78f5fa4c65710316f642acac84551e235
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-09-16 07:13:31 +0100

    Complete.

--- code/Data.java
+++ code/Data.java
@@ -6,9 +6,23 @@ package code;
  */
 public interface Data {
 	
+	/*
+	 * Returns true if the object referred to the by Data interface is of type
+	 * Instruction.
+	 * 
+	 * @return boolean true if Instruction, false otherwise.
+	 */
 	public boolean isInstruction();
 	
+	
+	/*
+	 * Returns true if the object referred to the by Data interface is of type
+	 * Operand, and that operand wraps an integer.
+	 * 
+	 * @return boolean true if the Operand wraps an integer, false otherwise.
+	 */
 	public boolean isInteger();
+	
 	
 	/*
 	 * Not currently in use but floating point operands could be introduced at some point.
