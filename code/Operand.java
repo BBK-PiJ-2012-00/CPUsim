11cab4e2f599b6a1129e5fb94ec95b5d498d2a94
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-09-16 07:16:08 +0100

    Complete.

--- code/Operand.java
+++ code/Operand.java
@@ -1,34 +1,47 @@
 package code;
 
 /*
- * A wrapper class for integers and floating point values; enables them to be stored in main memory.
+ * A wrapper class for integers (and floating point values); enables them to be stored in main memory.
  * 
  * Allows a degree of realism; in reality, values are stored in memory as a sequence of bits. Read one way, the bits form an
  * instruction, read another and they can form a (un)signed integer or floating point value. The Operand class allows integers
- * and floating point values to coexist in main memory, and also allow such values to be treated as either integers or floating
- * point values. Operands could be implemented as IntegerOperand, FloatingPointOperand etc, but this adds more redundant code and
- * unnecessary complexity.
+ * and floating point values to coexist in main memory, and also allows such values to be treated as either integers or floating
+ * point values.
  */
 public interface Operand extends Data {
 		
+	
+
+	/*
+	 * Unwraps an integer value stored in the field of the Operand.
+	 * 
+	 * @return int the wrapped integer value.
+	 */
 	public int unwrapInteger();
 	
+	
+	/*
+	 * Unwraps a floating point value stored in the field of the Operand.
+	 * 
+	 * @double the wrapped floating-point value.
+	 */
 	public double unwrapFloatingPoint();	
 
-	@Override
-	public boolean isInstruction();
-
-	@Override
-	public boolean isInteger();		
 	
+	@Override
+	public boolean isInstruction(); //Returns false for Operand types
+
+	
+	@Override
+	public boolean isInteger();	
+	
+	
+	/*
+	 * Not currently in use/
+	 */
+	@Override
 	public boolean isFloatingPoint();
 		
 
 }
 
-/*
-* A wrapper class to hold integer operands.  Implements data to allow for storage in main memory.
-* This helps manage and structure the data used in the simulator; all data extends Data at the top of the hierarchy,
-* with Instruction and Operands being on the second tier.  This avoids data in main memory being represented as type
-* Object, which is less specific. Casting isn't eliminated, but this approach is more belt and braces than using Object.
-*/
